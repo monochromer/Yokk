678f5631204c149638815ff934bc245ef3449009
max_buranbaev <max@izst.ru>
2016-09-20 17:37:27 +0200

    adding static, routes, models

--- index.js
+++ index.js
@@ -5,34 +5,36 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var mongoose = require('mongoose');
 
 
 // mongoose
-app.db = mongoose.createConnection(config.mongodb.uri);
+app.db = mongoose.createConnection('mongodb://localhost/test');
 app.db.on('error', console.error.bind(console, 'connection error:'));
 app.db.once('open', function() {
   console.log('App is now connected to MongoDB server');
 });
 
 
+// setting of models
+require('./models')(app, mongoose);
+
+
+//middleware
 app.use( bodyParser.urlencoded({ extended: true }) );
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-});
 
-app.post('/new_user', function(req, res) {
-    var user = new User(req.body);
-        user.save(function (err, user) {
-            if (err) return console.error(err);
-            res.send('Created new user: ' + user.name + ' ' + user.surname + ' is saved to DB');
-        });
-});
+// setting static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
+
+require('./routes')(app);
+
+
+// setting up
 const port = process.env.PORT ? process.env.PORT : 5000;
-
 app.listen(port, () => {
   console.log('App is listening on port ' + port);
 });
