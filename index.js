f449ba961b88bcb18db411e8c639e277bcae64c8
max_buranbaev <max@izst.ru>
2016-09-20 17:11:10 +0200

    code styling

--- index.js
+++ index.js
@@ -14,24 +14,25 @@ db.once('open', function() {
   console.log('App is now connected to MongoDB server');
 });
 
-var userSchema = mongoose.Schema(require('./mongo_model'));
+var userSchema = mongoose.Schema( require('./mongo_model') );
 var User = mongoose.model('User', userSchema);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use( bodyParser.urlencoded({ extended: true }) );
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname+'/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.post('/new_user', function(req, res) {
     var user = new User(req.body);
         user.save(function (err, user) {
             if (err) return console.error(err);
-            res.send('Created new user: '+user.name+' '+user.surname+' is saved to DB');
+            res.send('Created new user: ' + user.name + ' ' + user.surname + ' is saved to DB');
         });
 });
 
 const port = process.env.PORT ? process.env.PORT : 5000;
+
 app.listen(port, () => {
   console.log('App is listening on port ' + port);
 });
