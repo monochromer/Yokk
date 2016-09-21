e824f08963a470afd59a083fb01f22398d0b8998
max_buranbaev <max@izst.ru>
2016-09-21 17:44:51 +0200

    adding new routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import Layout from './components/Layout.jsx'
 import UserTable from './components/UserTable.jsx'
 import addUser from './components/AddUser.jsx'
 
 const router = (
-	<Router history={ browserHistory }>
+	<Router history={ hashHistory }>
 		<Route path="/" component={ Layout }>
 			<IndexRoute component={ UserTable }></IndexRoute>
-			<Route path="/addUser/" component={ addUser }></Route>
+			<Route path="addUser" component={ addUser }></Route>
 		</Route>
 	</Router>
 )
