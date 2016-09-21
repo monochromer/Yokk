6fbcd541d123a48bf474e2826fb115e67c4b5279
olegzhermal <olegzhermal@gmail.com>
2016-09-21 15:17:03 +0500

    understanding redux and axios logic

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import LoginForm from './components/LoginForm.jsx'
+import AddMember from './components/AddMember.jsx'
 
 var App = React.createClass({
   render: function() {
     return (
+        <div>
+        <AddMember />
         <LoginForm />
+        </div>
     );
   }
 });
--- public/js/components/AddMember.jsx
+++ public/js/components/AddMember.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
 
 var AddMember = React.createClass({
-  render: function() {
-    return (
-        <div>
-            <form>
-                <div>
-                    <h4 class="text-center">Add a member</h4>
-                    <label>Login
-                        <input type="text" placeholder="Some login" />
-                    </label>
-                    <label>Password
-                        <input type="text" placeholder="Some password" />
-                    </label>
-                    <label>Repeat password
-                        <input type="text" placeholder="Password typed" />
-                    </label>
-                    <p><a type="submit">Add user</a></p>
+    addUser: function(){
+        console.log('adding new user');
+    },
+    render: function() {
+        return (
+            <div className = 'row'>
+                <div className="small-3 small-centered columns">
+                    <form>
+                        <div className='row column add-user-form'>
+                        <div>
+                            <h4 className="text-center">Add a member</h4>
+                            <label>Login
+                                <input type="text" placeholder="Some login" />
+                            </label>
+                            <label>Password
+                                <input type="text" placeholder="Some password" />
+                            </label>
+                            <label>Repeat password
+                                <input type="text" placeholder="Password typed" />
+                            </label>
+                            <button type="submit" name="adduser" onClick={this.addUser}>Add user</button>
+                        </div>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
-    );
-  }
+            </div>
+        );
+    }
 });
 
 export default AddMember;
