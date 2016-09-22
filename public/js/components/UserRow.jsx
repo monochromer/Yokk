423e775f599cd1ab5b386033500e52e62312c488
max_buranbaev <max@izst.ru>
2016-09-22 17:54:07 +0200

    #2454 adding Links into component

--- public/js/components/UserRow.jsx
+++ public/js/components/UserRow.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router'
 
 var UserRow = React.createClass({
 	render: function() {
 		return (
 			<tr>
-				<td>{ this.props.name }</td>
+				<td><Link  to={ '/user/' + this.props.name }>{ this.props.name }</Link></td>
 				<td>{ this.props.position }</td>
 				<td>{ this.props.joinedOn }</td>
 				<td>Some actions...</td>
