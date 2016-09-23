164327a7bde2d3ad97f0dbcc6378d354d8f2aef9
max_buranbaev <max@izst.ru>
2016-09-23 10:29:39 +0200

    #2454 adding two new buttons for actions

--- public/js/components/UserRow.jsx
+++ public/js/components/UserRow.jsx
@@ -8,7 +8,12 @@ var UserRow = React.createClass({
 				<td><Link  to={ '/user/' + this.props.name }>{ this.props.name }</Link></td>
 				<td>{ this.props.position }</td>
 				<td>{ this.props.joinedOn }</td>
-				<td>Some actions...</td>
+				<td>
+					<div className="btn-group" role="group">
+						<Link  to={ '/user/edit/' + this.props.name } className="btn btn-warning">Edit</Link>
+						<button className="btn btn-danger">Remove</button>
+					</div>	
+				</td>
 			</tr>	
 		)
 	}
