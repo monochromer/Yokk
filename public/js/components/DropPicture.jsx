1f3f4146c5fcdea4951963f0e25b96ade2e445e4
max_buranbaev <max@izst.ru>
2016-09-27 10:39:04 +0200

    #2454 moving uploading logic to actions

--- public/js/components/DropPicture.jsx
+++ public/js/components/DropPicture.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Dropzone from 'react-dropzone';
-import request from 'superagent';
+import store from '../store.js';
+import { uploadUserPhoto } from '../actions/crudUser.js';
 
 var DropPicture = React.createClass({
-    onDrop: function (files) {
-        var file = files[0];
-        var postUrl = '/upload_profile_picture/users/' + this.props.login;
 
-        request
-            .post(postUrl)
-            .attach('pic', file)
-            .end(function(err, res) {
-                console.log(res);
-            });
+    onDrop: function (files) {
+        store.dispatch(uploadUserPhoto(files, this.props.login));
     },
+
     render: function () {
         return (
             <Dropzone onDrop={ this.onDrop }>
