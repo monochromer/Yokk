e74d91ed314588b915b38fc058c80b01dcaec5c9
max_buranbaev <max@izst.ru>
2016-09-27 10:06:17 +0200

    #2465 adding deleting reducer

--- public/js/reducer.js
+++ public/js/reducer.js
@@ -6,12 +6,29 @@ var initialState = {
 		visible: false,
 		class: "",
 		text: ""
+	},
+	modalDelete: {
+		visible: false,
+		login: ""
 	}
 }
 
 var Reducer = function(state = initialState, action) {
 	switch (action.type) {
 		
+		case "MODAL_DELETE_SHOW": 
+			var modalDelete = {
+				visible: true,
+				login: action.login
+			}
+			return Object.assign({}, state, { modalDelete: modalDelete }); 
+			break;
+
+		case "MODAL_DELETE_CLOSE":
+			var modalDelete = { visible: false };
+			return Object.assign({}, state, { modalDelete: modalDelete });
+			break; 	
+
 		case "ALERT_SHOW": 
 			var alert = {
 				visible: true,
@@ -36,6 +53,11 @@ var Reducer = function(state = initialState, action) {
 			newState.users.push(action.payload);
 			return newState;
 			break;
+
+		case "DELETE_USER":
+			var newState = Object.assign({}, state);
+			newState.users = _.filter(state.users, (o) => o.login != action.payload);
+			return newState; 	
 
 		case "CHANGE_USER":
 			var newState = Object.assign({}, state);
