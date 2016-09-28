09b1c3c19c9ad1b8fd31e3b209897913601d0adf
max_buranbaev <max@izst.ru>
2016-09-28 12:07:00 +0200

    #2460 moving navbar to another component

--- public/js/components/Layout.jsx
+++ public/js/components/Layout.jsx
@@ -2,31 +2,22 @@ import React from 'react';
 import { Link } from 'react-router';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import store from '../store.js';
-import { fetchUsers } from '../actions/crudUser';
-import Alert from './Alert.jsx'
-import ModalUserDelete from './ModalUserDelete.jsx'
+import { fetchUsers, checkPermissions } from '../actions/crudUser';
+import Alert from './Alert.jsx';
+import ModalUserDelete from './ModalUserDelete.jsx';
+import TopPanel from './TopPanel.jsx';
 
 var Layout = React.createClass({
 
     componentWillMount: function() {
         store.dispatch(fetchUsers());
+        store.dispatch(checkPermissions());
     },
 
 	render: function() {
 		return (
 			<div>
-				<nav className="navbar navbar-default">
-					<div className="container-fluid">
-						<div className="navbar-header">
-							<Link className="navbar-brand" to="/">Eye of providence</Link>
-						</div>
-						<ul className="nav navbar-nav">
-						  	<li><Link to="/">Team</Link></li>
-						  	<li><Link to="/addUser">Add User</Link></li>
-						  	<li><Link to="/tests">tests</Link></li>
-						</ul>
-					</div>
-				</nav>
+				<TopPanel />
 				<div className="container-fluid">
 					<Alert />
 					<ModalUserDelete />
