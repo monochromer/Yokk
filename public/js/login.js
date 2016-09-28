a99342ab9226ce1ca3fca8d28760bea7b678471f
max_buranbaev <max@izst.ru>
2016-09-28 10:29:25 +0200

    #2461 new login form

--- public/js/login.js
+++ public/js/login.js
@@ -3,37 +3,32 @@ import ReactDOM from 'react-dom';
 import request from 'superagent';
 
 var LoginForm = React.createClass({
-	getInitialState: function() {
-    return {login: 'foo', password:'bar'};
-	  },
-    onChange: function (event){
-		 event.preventDefault();
-	// 	 console.log(e.target.id);
-    //    this.setState({e.target.id: e.target.value}) ;
-     },
-     onSubmit: function(event){
-       event.preventDefault();
-     },
-     render: fuction() {
-       return <div>
-          <form>
-            <div className="form-group">
-              <label>Login</label>
-              <input type="text" id="login" value={this.state.login} onChange={this.onChange} className="form-control" placeholder="Login" />
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input type="password" id="password" value={this.state.password} onChange={this.onChange}  className="form-control" placeholder="Password" />
-            </div>
-            <div className="checkbox">
-              <label>
-                <input type="checkbox" onChange={this.onChange}  /> Logout
-              </label>
-            </div>
-            <button type="submit" onClick={this.onSubmit} className="btn btn-default">{options.submitButton.text}</button>
-          </form>
-        </div>
-     }
+
+	onChange: function (event) {
+		event.preventDefault();
+	},
+
+	onSubmit: function(event) {
+		event.preventDefault();
+	},
+
+	render: function() {
+		return (
+			<div className="container">
+				<form className="form-signin" method="POST" action="/login">
+        			<h2 className="form-signin-heading">Please sign in</h2>
+        			<input type="text" className="form-control" placeholder="Username" required="true" name="username" />
+        			<input type="password" className="form-control" placeholder="Password" required="true" name="password"/>
+        			<div className="checkbox">
+	          			<label>
+	            			<input type="checkbox" value="remember-me"/> Remember me
+	          			</label>
+        			</div>
+        			<button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      			</form>
+      		</div>
+		) 
+	  }
 });
 
 
