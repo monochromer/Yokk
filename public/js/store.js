243cf712c64a5943dede63e35fffcc1a5288cfaf
max_buranbaev <max@izst.ru>
2016-09-29 15:44:57 +0200

    #2466 some codestyling

--- public/js/store.js
+++ public/js/store.js
@@ -1,8 +1,8 @@
-import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import reducer from './reducer';
-import promise from 'redux-promise-middleware';
+import { applyMiddleware, createStore } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
+import promise from 'redux-promise-middleware'
 
 const middleware = applyMiddleware(promise(), thunk, logger());
 const store = createStore(reducer, middleware);
