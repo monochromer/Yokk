e49415a2811df21dcc197c3de0dda46688cf998d
olegzhermal <olegzhermal@gmail.com>
2016-09-29 19:48:39 +0500

    #2466 File upload now supports all picture extensions. File upload path changed

--- helpers/file_upload.js
+++ helpers/file_upload.js
@@ -1,11 +1,26 @@
 'use strict'
 
 var multer = require('multer');
+var moment = require('moment');
+var fs = require('fs');
+var path = require('path')
+var baseDir = path.join(__dirname, '../uploads/users/');
 
 var storage = multer.diskStorage({
-    destination: './uploads/users/',
+    destination: function(req, file, cb) {
+        var userDir = baseDir + req.params.user_login;
+        var avatarDir = userDir + '/avatars/';
+        if (!fs.existsSync(userDir)) {
+            fs.mkdirSync(userDir);
+            fs.mkdirSync(avatarDir);
+        }
+        if (!fs.existsSync(avatarDir)) {
+            fs.mkdirSync(avatarDir);
+        }
+        cb(null, avatarDir);
+    },
     filename: function(req, file, cb) {
-        cb(null, req.params.user_login + '-' + Date.now() + '.jpg');
+        cb(null, moment().format() + path.extname(file.originalname));
     }
 });
 
