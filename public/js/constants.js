528bc793ceebfa82e9ea6707dc09abc6a247b4bf
max_buranbaev <max@izst.ru>
2016-09-30 17:53:11 +0200

    #2466 adding new constants

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,3 +1,9 @@
 export const FETCH_USERS_URI = '/api/user'
 export const FETCH_CURRENT_USER_URI = '/api/user/check_permissions'
 export const UPDATE_USER_URI = '/api/user/'
+export const DELETE_USER_URI = '/api/user/'
+export const ADD_USER_URI = '/api/user/add'
+
+export function COMBINE_USER_ADDPHOTO_URI(login) {
+    return '/api/user/' + login + '/upload_profile_picture';
+}
