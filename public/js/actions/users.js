711f3787e6edcc9111bb39129c1b4439880d5858
max_buranbaev <max@izst.ru>
2016-09-30 12:19:11 +0200

    #2466 adding method for updating user

--- public/js/actions/users.js
+++ public/js/actions/users.js
@@ -1,8 +1,18 @@
-import { FETCH_USERS_URI } from '../constants'
+import { FETCH_USERS_URI, UPDATE_USER_URI } from '../constants'
 
 export function fetchUsers() {
     return {
         type: "FETCH_USERS",
         loadItems: FETCH_USERS_URI
+    }
+}
+
+export function updateUser(user) {
+    return {
+        type: "UPDATE_USER",
+        updateItem: {
+            url: UPDATE_USER_URI + user.login,
+            data: user
+        }
     }
 }
