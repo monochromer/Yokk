bc064bf983473f1fcdfbf0c9f376284ed55841db
max_buranbaev <max@izst.ru>
2016-09-30 12:20:18 +0200

    #2466 update_user_uri

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,2 +1,3 @@
 export const FETCH_USERS_URI = '/api/user'
-export const FETCH_CURRENT_USER_URI = '/api/check_permissions'
+export const FETCH_CURRENT_USER_URI = '/api/user/check_permissions'
+export const UPDATE_USER_URI = '/api/user/'
