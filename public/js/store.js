e8d6b0dd89d319bc4865303054a2c9e88aff2b97
max_buranbaev <max@izst.ru>
2016-09-30 11:06:08 +0200

    #2466 adding new middleware

--- public/js/store.js
+++ public/js/store.js
@@ -3,7 +3,8 @@ import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import reducer from './reducers'
 import promise from 'redux-promise-middleware'
+import loadItems from './middlewares/loadItems'
 
-const middleware = applyMiddleware(promise(), thunk, logger());
+const middleware = applyMiddleware(promise(), thunk, logger(), loadItems);
 const store = createStore(reducer, middleware);
 export default store;
