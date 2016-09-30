b27bdfe23464f4b02078810f498cbf4b2d58c133
max_buranbaev <max@izst.ru>
2016-09-30 17:54:56 +0200

    #2466 adding deleteItem Middleware

--- public/js/store.js
+++ public/js/store.js
@@ -5,7 +5,8 @@ import reducer from './reducers'
 import promise from 'redux-promise-middleware'
 import loadItems from './middlewares/loadItems'
 import updateItem from './middlewares/updateItem'
+import deleteItem from './middlewares/deleteItem'
 
-const middleware = applyMiddleware(promise(), thunk, logger(), loadItems, updateItem);
+const middleware = applyMiddleware(promise(), thunk, logger(), loadItems, updateItem, deleteItem);
 const store = createStore(reducer, middleware);
 export default store;
