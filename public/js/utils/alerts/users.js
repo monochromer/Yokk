1774aa4b3625603fa9b95d6940fd9cfcdb7598ff
max_buranbaev <max@izst.ru>
2016-09-30 17:55:52 +0200

    #2466 messages for alerts

---
+++ public/js/utils/alerts/users.js
@@ -0,0 +1,11 @@
+export function addUserSuccess(login) {
+    return 'Success! User "' + login + '" added.';
+}
+
+export function addPhotoSuccess(login) {
+    return "Photo of " + login + " has been succesfully uploaded.";
+}
+
+export function updateUserSuccess(login) {
+    return "User " + login + " has been succesfully changed.";
+}
