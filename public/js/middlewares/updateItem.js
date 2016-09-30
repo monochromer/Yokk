2b7339ff21151e5b0fcd8f461d451ecde32c5995
max_buranbaev <max@izst.ru>
2016-09-30 12:20:40 +0200

    #2466 middleware for updating items

---
+++ public/js/middlewares/updateItem.js
@@ -0,0 +1,13 @@
+import store from '../store'
+import axios from 'axios'
+
+export default(store) => (next) => (action) => {
+    const { updateItem } = action;
+    console.log("MIDDLEWARE RUNNING");
+    if (!updateItem) return next(action);
+
+    axios.put(updateItem.url, updateItem.data).then((response) => {
+        action.payload = response.data;
+        return next(action);
+    });
+}
