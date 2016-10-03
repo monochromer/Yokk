1c4f7ea5c9202ec58ed8c237a9255252df3a3df6
max_buranbaev <max@izst.ru>
2016-10-03 15:03:09 +0200

    #2518 creating index component for tracking

---
+++ public/js/components/tracking/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+var Tracking = React.createClass({
+    render: function() {
+        return (
+            <h1>This is tracking</h1>
+        )
+    }
+})
+
+export default Tracking
