7097ebac2e8f8520fa1bf3441ecef97e2621bf39
max_buranbaev <max@izst.ru>
2016-10-03 16:11:58 +0200

    #2519 new constant for add creating issue

--- public/js/constants.js
+++ public/js/constants.js
@@ -4,6 +4,7 @@ export const UPDATE_USER_URI = '/api/user/'
 export const DELETE_USER_URI = '/api/user/'
 export const ADD_USER_URI = '/api/user/add'
 export const FETCH_ISSUES_URI = '/redmine/issues'
+export const CREATE_ISSUE_URI = '/api/task/add'
 
 export function COMBINE_USER_ADDPHOTO_URI(login) {
     return '/api/user/' + login + '/upload_profile_picture';
