afc7c0b17a47ddabb4dcc186679568ab4e507f7f
max_buranbaev <max@izst.ru>
2016-10-03 21:18:59 +0200

    #2519 new format of duration

--- public/js/components/tracking/index.jsx
+++ public/js/components/tracking/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import store from '../../store.js'
+import moment from 'moment'
 import { createIssue } from '../../actions/issues.js'
+import { connect } from 'react-redux'
+import { refsToObject } from '../../helpers'
 
 var Tracking = React.createClass({
-    handleSubmit: function(e) {
 
-    },
 
     render: function() {
         return (
@@ -17,32 +19,34 @@ var Tracking = React.createClass({
                     </div>
                 </div>
                 <div className="row">
-                    <form>
+                    <form onSubmit={ this.handleSubmit }>
+                        <input type="hidden" ref="executor" value={ this.props.currentUser } />
+                        <input type="hidden" ref="taskSource" value="eop" />
                         <div className="col-md-7">
                             <div className="form-group">
                                 <label htmlFor="date">Task</label>
-                                <input type="text" className="form-control" placeholder="What are you working on?"/>
+                                <input type="text" className="form-control" ref="description" placeholder="What are you working on?"/>
                             </div>
                         </div>
                         <div className="col-md-2">
                             <div className="form-group">
                                 <label htmlFor="date">Date</label>
-                                <input type="text" className="form-control" id="date" defaultValue="22.05.1014"/>
+                                <input type="text" className="form-control" ref="dateAdded" id="date" defaultValue="22.05.1014"/>
                             </div>
                         </div>
                         <div className="col-md-2">
                             <div className="form-group">
                                 <label htmlFor="time">Time</label>
-                                <select className="form-control" id="time" defaultValue="22.05.1014">
-                                    <option value="0:30" default>0:30</option>
-                                    <option value="1:00">1:00</option>
-                                    <option value="1:30">1:30</option>
-                                    <option value="2:00">2:00</option>
-                                    <option value="2:30">2:30</option>
-                                    <option value="3:00">3:00</option>
-                                    <option value="3:30">3:30</option>
-                                    <option value="4:00">4:00</option>
-                                    <option value="4:30">4:30</option>
+                                <select className="form-control" id="time" ref="minutesSpent" defaultValue="22.05.1014">
+                                    <option value="00:30" default>0:30</option>
+                                    <option value="01:00">01:00</option>
+                                    <option value="01:30">01:30</option>
+                                    <option value="02:00">02:00</option>
+                                    <option value="02:30">02:30</option>
+                                    <option value="03:00">03:00</option>
+                                    <option value="03:30">03:30</option>
+                                    <option value="04:00">04:00</option>
+                                    <option value="04:30">04:30</option>
                                 </select>
                             </div>
                         </div>
