a14ed5108191b0de1726a9edd04e1d4e6bc61a3b
max_buranbaev <max@izst.ru>
2016-10-03 16:11:01 +0200

    #2519 new method for adding issue

--- public/js/actions/issues.js
+++ public/js/actions/issues.js
@@ -1,8 +1,18 @@
-import { FETCH_ISSUES_URI } from '../constants'
+import { FETCH_ISSUES_URI, CREATE_ISSUE_URI } from '../constants'
 
 export function fetchIssues() {
     return {
         type: "FETCH_ISSUES",
         loadItems: FETCH_ISSUES_URI
+    }
+}
+
+export function createIssue(issue) {
+    return {
+        type: "CREATE_ISSUE",
+        createItem: {
+            url: "CREATE_ISSUE_URI",
+            data: issue
+        }
     }
 }
