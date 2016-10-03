b3f857636e60d3304058d26b3a66d4faa2d82936
max_buranbaev <max@izst.ru>
2016-10-03 20:58:39 +0200

    #2519 bugfix

--- public/js/actions/issues.js
+++ public/js/actions/issues.js
@@ -11,7 +11,7 @@ export function createIssue(issue) {
     return {
         type: "CREATE_ISSUE",
         createItem: {
-            url: "CREATE_ISSUE_URI",
+            url: CREATE_ISSUE_URI,
             data: issue
         }
     }
