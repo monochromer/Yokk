da4b8893fcadf585bdc90005d4ca4e0eebc10d62
max_buranbaev <max@izst.ru>
2016-10-03 15:01:01 +0200

    #2518 adding component "tracking"
    - adding new component
    - changing paths of user components

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import store from './store.js'
 import Layout from './components/Layout.jsx'
-import UserTable from './components/UserTable.jsx'
-import UserAdd from './components/UserAdd.jsx'
-import UserPage from './components/UserPage.jsx'
-import UserEdit from './components/UserEdit.jsx'
+import UserTable from './components/user/UserTable.jsx'
+import UserAdd from './components/user/UserAdd.jsx'
+import UserPage from './components/user/UserPage.jsx'
+import UserEdit from './components/user/UserEdit.jsx'
+import Tracking from './components/tracking'
 import RequestTest from './components/RequestTest.jsx'
 import RedmineIssuesTable from './components/RedmineIssuesTable.jsx'
 
@@ -16,7 +17,8 @@ const router = (
 	<Provider store={ store }>
 		<Router history={ browserHistory }>
 			<Route path="/" component={ Layout }>
-				<IndexRoute component={ UserTable }></IndexRoute>
+				<IndexRoute component={ Tracking }></IndexRoute>
+				<Route path="users" component={ UserTable }></Route>
 				<Route path="addUser" component={ UserAdd }></Route>
 				<Route path="user/:login" component={ UserPage }></Route>
 				<Route path="user/edit/:login" component={ UserEdit }></Route>
