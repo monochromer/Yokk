1ed3622bef2de1ab148d0c96020f4beeadcaa363
max_buranbaev <max@izst.ru>
2016-10-04 10:22:42 +0200

    #2520 returning html at issuesList

--- public/js/components/tracking/IssuesList.jsx
+++ public/js/components/tracking/IssuesList.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 var IssuesList = React.createClass({
     render: function() {
-        <div>
-            <div className="row">
-                <div className="col-md-12">
-                    
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1>Hi!</h1>
+                    </div>
                 </div>
             </div>
-        </div>
+        )    
     }
 });
 
