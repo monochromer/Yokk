f9bcea284376d2edfc92d2dcf6bdf18fee77c6c8
max_buranbaev <max@izst.ru>
2016-10-04 17:59:11 +0200

    #2520 getting issues and rendering

--- public/js/components/tracking/IssuesList.jsx
+++ public/js/components/tracking/IssuesList.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
+import IssuesPerDay from './IssuesPerDay.jsx'
 import store from '../../store'
+import moment from 'moment'
 import { fetchIssues } from '../../actions/issues'
 import { connect } from 'react-redux'
+import { dayBeatify, durationBeatify } from '../../helpers'
 
 var IssuesList = React.createClass({
     componentWillMount: function() {
         store.dispatch(fetchIssues());
     },
+
     render: function() {
+        const { issues } = this.props;
+        var rows = [];
+        for( var date in issues) {
+            var day = dayBeatify(date, "DDMMYYYY");
+            var duration = durationBeatify(issues[date].totalDuration);
+            rows.push(<IssuesPerDay day={ day } duration={ duration } issues={ issues[date].list } key={ date } />)
+        }
         return (
             <div>
-
+                { rows }
             </div>
         )
     }
 });
 
-var getProps = function(store) {
+var getProps = function(state) {
         return {
-            issues: store.issues.list
+            issues: state.issues.list
         }
 }
 
