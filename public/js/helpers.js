ed9b176a6ea3fe14873e3a82528ecc8de9a59881
max_buranbaev <max@izst.ru>
2016-10-04 18:00:14 +0200

    #2520 date beatifyers

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,4 +1,5 @@
-import _ from "lodash";
+import _ from "lodash"
+import moment from 'moment'
 
 export function findUserByLogin(users, login) {
 	return _.find(users, (o) => o.login == login);
@@ -10,6 +11,15 @@ export function refsToObject(refs) {
 	for(var field in refs) {
 		data[field] = refs[field].value;
 	}
-	
+
 	return data;
+}
+
+export function dayBeatify(date, format) {
+	return moment(date, format).format("ddd, D MMM")
+}
+
+export function durationBeatify(minutes) {
+	var duration = moment.duration(minutes, 'minutes');
+	return duration.get('hours') + " h " + duration.get('minutes') + " min";
 }
