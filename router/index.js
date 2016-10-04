40fd3e96edb295e32d8be4de16025d2a6f281175
olegzhermal <olegzhermal@gmail.com>
2016-10-04 13:17:13 +0500

    minor refactoring

--- router/index.js
+++ router/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const authorization = require('./authorization');
+const checkAuthStatus = require('connect-ensure-login').ensureLoggedIn();
 const userAPI = require('./userAPI');
 const taskAPI = require('./taskAPI');
 const helperRoutes = require('./helperRoutes');
@@ -9,25 +10,25 @@ const upload = require('../helpers/file_upload');
 
 module.exports = function(app, passport) {
 
-    app.get('/', require('connect-ensure-login').ensureLoggedIn(), authorization.index);
+    app.get('/', checkAuthStatus, authorization.index);
     app.get('/login', authorization.login);
     app.post('/login', passport.authenticate('local', {
         failureRedirect: '/login'
     }), authorization.auth);
     app.get('/logout', authorization.logout);
 
-    app.get('/api/user', require('connect-ensure-login').ensureLoggedIn(), userAPI.getAllUsers);
-    app.post('/api/user/add', require('connect-ensure-login').ensureLoggedIn(), userAPI.saveUserToDb);
+    app.get('/api/user', checkAuthStatus, userAPI.getAllUsers);
+    app.post('/api/user/add', checkAuthStatus, userAPI.saveUserToDb);
     app.get('/api/user/check_permissions', userAPI.checkUserPermissions); //can cause conflicts when login is 'check_permissions'
-    app.get('/api/user/:user_login', require('connect-ensure-login').ensureLoggedIn(), userAPI.showUser);
-    app.put('/api/user/:user_login', require('connect-ensure-login').ensureLoggedIn(), userAPI.updateUser);
-    app.delete('/api/user/:user_login', require('connect-ensure-login').ensureLoggedIn(), userAPI.deleteUser);
+    app.get('/api/user/:user_login', checkAuthStatus, userAPI.showUser);
+    app.put('/api/user/:user_login', checkAuthStatus, userAPI.updateUser);
+    app.delete('/api/user/:user_login', checkAuthStatus, userAPI.deleteUser);
     app.post('/api/user/:user_login/upload_profile_picture', upload.single('pic'), userAPI.uploadUserAvatar);
 
-    app.get('/api/task', taskAPI.projectTasks);
-    app.post('/api/task/add', taskAPI.saveTask); // body should contain: executor, description, taskSource
-    app.delete('/api/task/:taskNumber', require('connect-ensure-login').ensureLoggedIn(), taskAPI.deleteTask);
-    app.put('/api/task/:taskNumber', require('connect-ensure-login').ensureLoggedIn(), taskAPI.updateTask);
+    app.get('/api/task', checkAuthStatus, taskAPI.projectTasks);
+    app.post('/api/task/add', checkAuthStatus, taskAPI.saveTask); // body should contain: executor, description, taskSource
+    app.delete('/api/task/:taskNumber', checkAuthStatus, taskAPI.deleteTask);
+    app.put('/api/task/:taskNumber', checkAuthStatus, taskAPI.updateTask);
 
     app.get('/redmine/issues', redmine.issues); // add /limit/:num
     app.get('/redmine/projects', redmine.projects); // add /limit/:num
@@ -35,6 +36,6 @@ module.exports = function(app, passport) {
     app.get('/redmine/current_user', redmine.currentUser);
 
 
-    app.get('*', require('connect-ensure-login').ensureLoggedIn(), helperRoutes.redirectUndefinedRoutes);
+    app.get('*', checkAuthStatus, helperRoutes.redirectUndefinedRoutes);
 
 }
