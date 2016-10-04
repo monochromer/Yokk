272d35bd51c3f19d84d30c013ffc74b8923c8178
max_buranbaev <max@izst.ru>
2016-10-04 10:58:46 +0200

    #2520 table for each day

--- public/js/components/tracking/IssuesList.jsx
+++ public/js/components/tracking/IssuesList.jsx
@@ -6,11 +6,39 @@ var IssuesList = React.createClass({
             <div>
                 <div className="row">
                     <div className="col-md-12">
-                        <h1>Hi!</h1>
+                        <div className="row day">
+                            <div className="col-md-4 day__date">
+                                <h3>Fri, 20 Sep
+                                    <span className="day__total-duration"> 5 h 20 min</span>
+                                </h3>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <td></td>
+                                            <td>Task</td>
+                                            <td>Duration</td>
+                                            <td>Actions</td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td></td>
+                                            <td>Implemeting super features</td>
+                                            <td>0:30</td>
+                                            <td></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        )    
+        )
     }
 });
 
