61e9a6abbc6b010266a64e2cd76d9e514963bb6a
olegzhermal <olegzhermal@gmail.com>
2016-10-05 14:03:18 +0500

    #2505 Data is retrieved and pushed in an array that fit model
    Problems:
    When retrieving an issues, there is no duration field. Should be
    retrieved by an extra request time_entries
    
    The task is still in development

--- router/redmine.js
+++ router/redmine.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const redmineObject = require('node-redmine');
+const moment = require('moment');
+const assignDefined = require('./helpers/assignDefined');
 
 var hostname = process.env.REDMINE_HOST || 'redmine.soshace.com';
 var config = {
@@ -9,20 +11,79 @@ var config = {
 
 var redmine = new redmineObject(hostname, config);
 
+
 exports.issues = function(req, res) {
-    // res.send(req.params);
-    // params: offset, limit, sort
-    var params = req.query;
-    console.log(params);
-    redmine.issues(params = {}, function(err, data) {
+    const params = req.query;
+    const id = parseInt(params.id);
+    var dataToReturn = {};
+
+    // params:
+    // limit: Num (number of issues per page)
+    // offset: Num (skip this number of issues in response)
+    // sort: ColumntName (or sort:desc)
+    // FILTERS: project_id, subproject_id, tracker_id, status_id, assigned_to_id ...
+    // created_on: date (TODO: how to give a period?)
+
+    redmine.issues(params, function(err, data) {
         if (err) throw err;
 
-        // for (var i in data.issues) {
-        //     console.log(data.issues[i]);
-        // }
+        data.issues.forEach((element) => {
+            let task = {};
+            task.taskNumber = element.id;
+
+            if (typeof element.assigned_to === 'undefined') {
+                task.exectutor = 'not assigned';
+            } else {
+                task.exectutor = element.assigned_to.name;
+            };
+
+            task.dateAdded = moment(element.created_on, 'YYYY-MM-DD').toDate();
+
+            if (typeof element.start_date === 'undefined') {
+                task.startDate = 'not assigned';
+            } else {
+                task.startDate = moment(element.start_date, 'YYYY-MM-DD').toDate();
+            };
+
+            if (typeof element.due_date === 'undefined') {
+                task.endDate = 'not assigned';
+            } else {
+                task.endDate = moment(element.due_date, 'YYYY-MM-DD').toDate();
+            };
+
+            task.description = element.subject;
+            task.taskSource = "redmine";
+            if (!dataToReturn.tasks) dataToReturn.tasks = [];
+            dataToReturn.tasks.push(task);
+        });
+
+        // dataToReturn.tasks2 = [];
+        // dataToReturn.tasks.forEach((element) => {
+        //     redmine.get_issue_by_id(element.taskNumber, params, function(err, data) {
+        //         let task = {};
+        //         task.duration = data.issue.spent_hours;
+        //         dataToReturn.tasks2.push(task)
         //
-        // console.log('total_count: ' + data.total_count);
-        res.send(data);
+        //         console.log(task);
+        //     })
+        // })
+
+        redmine.time_entries((err,data)=> {
+          var arr = [];
+          data.time_entries.forEach((element)=>{
+
+            arr.push(element.hours);
+
+          })
+          res.send(dataToReturn);
+        });
+
+        var debugInfo = {};
+        debugInfo.params = params;
+        debugInfo.data = data;
+        dataToReturn.debugInfo = debugInfo;
+
+        // res.send(dataToReturn);
     });
 }
 
