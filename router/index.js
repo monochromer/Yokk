c42a47d1983b80f35d8ee6cf7fc549d4914121fd
olegzhermal <olegzhermal@gmail.com>
2016-10-05 13:03:19 +0500

    #2516 bug fixes

--- router/index.js
+++ router/index.js
@@ -28,8 +28,8 @@ module.exports = function(app, passport) {
     app.get('/api/task', checkAuthStatus, taskAPI.projectTasks);
     app.get('/api/task/duration', checkAuthStatus, taskAPI.totalDuration);
     app.post('/api/task/add', checkAuthStatus, taskAPI.saveTask); // body should contain: executor, description, taskSource
-    app.delete('/api/task/:taskNumber', checkAuthStatus, taskAPI.deleteTask);
-    app.put('/api/task/:taskNumber', checkAuthStatus, taskAPI.updateTask);
+    app.delete('/api/task/:taskId', checkAuthStatus, taskAPI.deleteTask);
+    app.put('/api/task/:taskId', checkAuthStatus, taskAPI.updateTask);
 
     app.get('/redmine/issues', redmine.issues); // add /limit/:num
     app.get('/redmine/projects', redmine.projects); // add /limit/:num
