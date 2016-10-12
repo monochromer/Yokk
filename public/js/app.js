9b5af844d3e5ad21a27b86acb4b8477b09ffebff
max_buranbaev <max@izst.ru>
2016-10-12 07:58:22 +0200

    #2556 profile added, new menu items

--- public/js/app.js
+++ public/js/app.js
@@ -16,8 +16,7 @@ const router = (
 		<Router history={ browserHistory }>
 			<Route path="/" component={ Layout }>
 				<IndexRoute component={ Tracking }></IndexRoute>
-				<Route path="tracking" component={ Tracking }></Route>
-				<Route path="users" component={ UserTable }></Route>
+				<Route path="/users" component={ UserTable }></Route>
 				<Route path="addUser" component={ UserAdd }></Route>
 				<Route path="user/:login" component={ UserPage }></Route>
 				<Route path="user/edit/:login" component={ UserEdit }></Route>
--- public/js/components/navbar/TopPanel.jsx
+++ public/js/components/navbar/TopPanel.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import Logout from './Logout.jsx'
 import { Link } from 'react-router'
-import { LinkContainer } from 'react-router-bootstrap'
+import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap'
 import { connect } from 'react-redux'
 import { Navbar, Nav, NavDropdown, MenuItem, NavItem } from 'react-bootstrap'
 
 var TopPanel = React.createClass({
 	render: function() {
 		return (
-			<Navbar>
+			<Navbar fluid>
 				<Navbar.Header>
 					<Navbar.Brand>
 						<Link className="navbar-brand" to="/">Eye of providence</Link>
 					</Navbar.Brand>
 				</Navbar.Header>
 				<Nav>
-					<LinkContainer to="/tracking/">
+					<IndexLinkContainer to="/">
 						<NavItem eventKey={1}>Tracking</NavItem>
-					</LinkContainer>
-					<LinkContainer to="/users/">
+					</IndexLinkContainer>
+					<LinkContainer to="/users">
 						<NavItem eventKey={2}>Team</NavItem>
 					</LinkContainer>
-					<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-						<MenuItem eventKey={3.1}>Your Profile</MenuItem>
-						<MenuItem eventKey={3.2}>Logout</MenuItem>
+				</Nav>
+				<Nav pullRight>
+					<NavDropdown eventKey={3} title={this.props.login} id="basic-nav-dropdown" >
+						<LinkContainer to={ "/user/edit/" + this.props.login }>
+							<MenuItem eventKey={3.1}>Your Profile</MenuItem>
+						</LinkContainer>
+						<MenuItem href="/logout" eventKey={3.2}>Logout</MenuItem>
 					</NavDropdown>
 				</Nav>
 			</Navbar>
