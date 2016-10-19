d2699530c00a8784e39eb347f36b3b7afb89f5f0
max_buranbaev <max@izst.ru>
2016-10-19 12:57:48 +0200

    #2582 adding next

--- index.js
+++ index.js
@@ -28,8 +28,9 @@ var passport = require('./helpers/userpassport')(app);
 require('./router')(app, passport);
 
 // error handler
-app.use( (err, req, res) => {
-    res.status(500).send(err.name);
+app.use( (err, req, res, next) => {
+    console.log(err);
+    res.status(500).send(err);
 });
 
 
