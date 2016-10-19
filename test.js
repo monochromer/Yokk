181e325d9eea9d8279aa11867e8ad8b2b95e6dc8
max_buranbaev <max@izst.ru>
2016-10-19 12:58:57 +0200

    #2580 adding tests for a user

--- test.js
+++ test.js
@@ -2,6 +2,9 @@ const app = require('./index');
 const should = require('should');
 const supertest = require('supertest');
 const request = require('request');
+const async = require('async');
+
+const randomUserAPI = "https://beta.randomapi.com/api/dimmc6ab?key=4LZJ-USXM-7MEN-K404";
 
 describe('TESTING TIME ENTRY API', () => {
 
@@ -85,59 +88,80 @@ describe('TESTING TIME ENTRY API', () => {
 
 describe('TESTING USERS API', () => {
 
-    var newUser = {};
-
-    request
-        .get("https://beta.randomapi.com/api/963d88fa5c96e2c6a620e55c9a7d6e26")
-        .on('response', (response) => {
-            newUser = response.results[0];
-        })
-        .on('error', (error) => console.log(err));
+    var userLogin = "";
 
     it('creating the new user', (done) => {
+        async.waterfall(
+            [
+                (callback) => {
+                    request(randomUserAPI, (error, response, body) => {
+                        var newUser = JSON.parse(body).results[0];
+                        userLogin = newUser.login;
+                        callback(null, newUser);
+                    });
+                },
+                (newUser) => {
+                    supertest(app)
+                        .post('/api/user/add')
+                        .send(newUser)
+                        .expect(200)
+                        .end((err, res) => {
+                            res.status.should.equal(200);
+                            res.body.email.should.equal(newUser.email);
+                            res.body.login.should.equal(newUser.login);
+                            res.body.role.should.equal('user');
+                            res.body.should.have.property('_id').which.is.a.String();
+                            done();
+                        });
 
-        supertest(app)
-            .post('/api/user/add')
-            .send('newUser')
-            .expect(200)
-            .end((err, res) => {
-                console.log(res);
-                res.status.should.equal(200);
-                res.body.email.should.equal(newUser.email);
-                res.body.login.should.equal(newUser.login);
-                res.body.role.should.equal('user');
-                res.body._id.should.have.property('_id').which.is.a.String();
-                done();
-            });
+                }
+            ]
+        );
     });
 
     it('updating the user', (done) => {
+        async.waterfall(
+            [
+                (callback) => {
+                    request(randomUserAPI, (error, response, body) => {
+                        var updatedUser = JSON.parse(body).results[0];
+                        delete updatedUser.login;
+                        delete updatedUser.password;
+                        callback(null, updatedUser);
+                    });
+                },
+                (updatedUser) => {
+                    supertest(app)
+                        .put('/api/user/' + userLogin)
+                        .send(updatedUser)
+                        .expect(200)
+                        .end((err, res) => {
+                            res.status.should.equal(200);
+                            res.body.redmineApiKey.should.equal(updatedUser.redmineApiKey);
+                            res.body.aboutme.should.equal(updatedUser.aboutme);
+                            res.body.vk.should.equal(updatedUser.vk);
+                            res.body.birthday.should.equal(updatedUser.birthday);
+                            res.body.email.should.equal(updatedUser.email);
+                            res.body.skype.should.equal(updatedUser.skype);
+                            res.body.phone.should.equal(updatedUser.phone);
+                            res.body.position.should.equal(updatedUser.position);
+                            res.body.fullname.should.equal(updatedUser.fullname);
+                            done();
+                        });
+                }
+            ]
+        );
+    });
 
-        var updatedUser = {};
-        request
-            .get("https://beta.randomapi.com/api/963d88fa5c96e2c6a620e55c9a7d6e26")
-            .on('response', (response) => {
-                updatedUser = response.results[0];
-                delete updatedUser.login;
-            })
-            .on('error', (error) => console.log(err));
-
+    it('deleting the user', (done) => {
         supertest(app)
-            .put('/api/user/' + newUser.login)
-            .send(updatedUser)
+            .delete('/api/user/' + userLogin)
             .expect(200)
             .end( (err, res) => {
                 res.status.should.equal(200);
-                res.body.redmineApiKey.should.equal(updatedUser.redmineApiKey);
-                res.body.aboutme.should.equal(updatedUser.aboutme);
-                res.body.vk.should.equal(updatedUser.vk);
-                res.body.birthday.should.equal(updatedUser.birthday);
-                res.body.email.should.equal(updatedUser.email);
-                res.body.skype.should.equal(updatedUser.skype);
-                res.body.skype.phone.equal(updatedUser.phone);
-                res.body.position.should.equal(updatedUser.position);
-                res.body.fullname.should.equal(updatedUser.fullname);
+                res.text.should.equal(userLogin);
                 done();
             });
+
     });
 });
