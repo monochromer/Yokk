bf79b1669dd8a90ded6b44f2c8789b0d493d3519
olegzhermal <olegzhermal@gmail.com>
2016-10-20 12:16:26 +0500

    #2575 Render duration in hours and minutes as a string

--- public/js/components/ReportsPage/ReportTableRow.jsx
+++ public/js/components/ReportsPage/ReportTableRow.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import store from '../../store.js';
+import {convertToHours} from './reportPageHelpers.js';
 
 class ReportTableRow extends React.Component {
     render() {
@@ -11,10 +12,10 @@ class ReportTableRow extends React.Component {
 
         if (typeof this.props.responseData !== 'undefined') {
             userName = this.props.userName;
-            totalDuration = this.props.responseData.total;
-            redmineDuration = this.props.responseData.redmine;
+            totalDuration = convertToHours(this.props.responseData.total);
+            redmineDuration = convertToHours(this.props.responseData.redmine);
             upworkDuration = 'feature is not implemented yet'
-            directDuration = this.props.responseData.eop;
+            directDuration = convertToHours(this.props.responseData.eop);
         } else {
             userName = totalDuration = redmineDuration = upworkDuration = directDuration = 'no data';
         }
--- public/js/components/ReportsPage/reportPageHelpers.js
+++ public/js/components/ReportsPage/reportPageHelpers.js
@@ -2,9 +2,16 @@ import _ from 'lodash';
 
 export function getCheckboxStatus(checkBoxStatus, loginToCheck) {
     if (typeof checkBoxStatus !== 'undefined') {
-      for (let login in checkBoxStatus) {
-        if (loginToCheck === login) return checkBoxStatus[login];
-      }
+        for (let login in checkBoxStatus) {
+            if (loginToCheck === login) return checkBoxStatus[login];
+        }
     }
     return false;
+}
+
+export function convertToHours(durationInMinutes, digit) {
+    const numberOfMinutes = durationInMinutes % 60;
+    const digitsAfterComma = digit || 0;
+    const numberOfHours = (durationInMinutes / 60).toFixed(digitsAfterComma)
+    return `${numberOfHours} h ${numberOfMinutes} min`;
 }
