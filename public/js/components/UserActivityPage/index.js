959f6a27811b2b334673e8a1726a019ba3e6900c
max_buranbaev <max@izst.ru>
2016-10-21 11:55:01 +0200

    #2582 adding jumbotron and avatar of the user

--- public/js/components/UserActivityPage/index.js
+++ public/js/components/UserActivityPage/index.js
@@ -1,27 +1,53 @@
 import React from 'react';
 import UserActivityTable from './UserActivityTable.jsx';
-import FilterPeriod from './FilterPeriod.jsx';
-import store from '../../store';
-import {connect} from 'react-redux';
-import {getUserFullName} from './helpers.js'
-// import dateRangePicker from './DateRangePicker.jsx';
-// require("./css/dateRangePicker.css");
+import { connect } from 'react-redux';
+import { findUserByLogin } from '../../helpers.js'
 
 class UserActivityPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userHeading: "",
+            photo: "",
+            position: ""
+        }
+    }
+
     render() {
-        const userPageRenderedFor = getUserFullName(this.props.routeParams.login, this.props.users)
+        var user = findUserByLogin(this.props.users, this.props.routeParams.login);
+
+        if(user) {
+            this.state.userHeading = user.fullname ? user.fullname : user.login;
+            this.state.photo = user.profileImg.medium;
+            this.state.position = user.position ? user.position : "Here is user's an activity";
+        }
+
         return (
             <div>
                 <div className="row">
-                    <div className="col-md-6">
-                        <h2>Activity Page for {userPageRenderedFor}</h2>
-                    </div>
-                    <div>
-                        <FilterPeriod/>
+                    <div className="col-md-12">
+                        <div className="page-header">
+                            <div className="jumbotron">
+                                <div className="row">
+                                    <div className="col-md-2">
+                                        <img src={ this.state.photo } width="200px" className="img-thumbnail"/>
+                                    </div>
+                                    <div className="col-md-8">
+                                        <h2>
+                                            { this.state.userHeading } <br />
+                                            <small>  { this.state.position } </small>
+                                        </h2>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="row">
-                    <UserActivityTable login={this.props.routeParams.login}/>
+                    <div className="col-md-12">
+                        <UserActivityTable login={this.props.routeParams.login}/>
+                    </div>
                 </div>
             </div>
         );
@@ -29,7 +55,9 @@ class UserActivityPage extends React.Component {
 }
 
 let getProps = function(state) {
-    return {users: state.users};
-}
+    return {
+        users: state.users
+    };
+};
 
 export default connect(getProps)(UserActivityPage);
