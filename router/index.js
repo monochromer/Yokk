c30c36f8ed81324133453a74c426f48c698344aa
max_buranbaev <max@izst.ru>
2016-10-24 19:38:19 +0200

    #2595 new route for main page

--- router/index.js
+++ router/index.js
@@ -3,7 +3,6 @@
 const authorization = require('./authorization');
 const checkAuthStatus = require('connect-ensure-login').ensureLoggedIn();
 const helperRoutes = require('./helperRoutes');
-const upload = require('../helpers/file_upload');
 const checkFrontPath = require('./frontPaths.js');
 const path = require('path');
 const registration = require('./register');
@@ -16,6 +15,7 @@ module.exports = function(app, passport) {
     app.post('/login', passport.authenticate('local', {
         failureRedirect: '/login'
     }), authorization.auth);
+
     app.get('/logout', authorization.logout);
 
     app.post('/register', registration, passport.authenticate('local', {
@@ -23,6 +23,8 @@ module.exports = function(app, passport) {
     }), authorization.auth);
 
     app.use('/api', api);
+
+    app.get('/mainpage', (req, res, next) => res.sendFile(path.resolve(__dirname, '../views', 'mainpage.html')));
 
     app.get('*', checkAuthStatus, checkFrontPath, helperRoutes.redirectUndefinedRoutes);
 
