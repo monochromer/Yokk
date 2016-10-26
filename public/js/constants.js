85141cafa65352cf64d0d9f0958e162d79b17850
max_buranbaev <max@izst.ru>
2016-10-26 10:53:33 +0200

    #2596 adding constant for the team API

--- public/js/constants.js
+++ public/js/constants.js
@@ -1,9 +1,15 @@
-export const FETCH_USERS_URI = '/api/user';
 export const FETCH_CURRENT_USER_URI = '/api/user/check_permissions';
+
 export const USER_CRUD = '/api/user/';
+
 export const TIME_ENTRY_CRUD = '/api/timeEntry/';
+
 export const FETCH_REDMINE_TIME_ENTRIES_URI = '/api/sync/redmine';
+
 export const REPORT_API_URI = '/api/report/';
+
+export const TEAM_CRUD = '/api/team';
+
 export function COMBINE_USER_ADDPHOTO_URI(login) {
     return '/api/user/' + login + '/upload_profile_picture';
 }
