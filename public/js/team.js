b3ac249b1a7ae77a288fabc54264f69c3237b6e7
max_buranbaev <max@izst.ru>
2016-10-26 17:25:59 +0200

    #2599 route for step4

--- public/js/team.js
+++ public/js/team.js
@@ -5,6 +5,7 @@ import Layout from './components/promo/team/Layout.jsx'
 import Step1 from './components/promo/team/Step1.jsx'
 import Step2 from './components/promo/team/Step2.jsx'
 import Step3 from './components/promo/team/Step3.jsx'
+import Step4 from './components/promo/team/Step4.jsx'
 import store from './store.js'
 
 
@@ -19,6 +20,7 @@ const router = (
                 <Route path="step1" component={ Step1 } />
                 <Route path="step2" component={ Step2 } />
                 <Route path="step3" component={ Step3 } />
+                <Route path="step4" component={ Step4 } />
             </Route>
         </Router>
     </Provider>
