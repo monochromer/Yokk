c811c366926fd6021cb0a2081371f5df241a9650
max_buranbaev <max@izst.ru>
2016-10-26 13:43:08 +0200

    #2596 scratch for step1

---
+++ public/js/components/promo/team/Step1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Step1 extends React.Component {
+    render() {
+        return (
+            <h1>This is Step 1</h1>
+        )
+    }
+}
+
+export default Step1
