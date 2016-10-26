615a5c1addb78a465185f230836d64cc7477a98f
max_buranbaev <max@izst.ru>
2016-10-26 16:54:24 +0200

    #2598 minimal viable component for Step3

---
+++ public/js/components/promo/team/Step3.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class Step3 extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row center-xs step__heading">
+                    <div className="col-md-6 col-sm-8 col-xs-10">
+                        <h1 className="heading">Set your password</h1>
+                    </div>
+                </div>
+                <div className="row center-xs step__message">
+                    <div className="col-md-6 col-sm-8 col-xs-10">
+                        <p>Password must be at least 6 characters long,<br/>
+                            and can’t be things like password, 123456 or abcdef</p>
+                    </div>
+                </div>
+                <div className="row center-xs step__code">
+                    <div className="col-md-6 col-sm-8 col-xs-10">
+                        <input className="input input_black" type="password" placeholder="Password"/>
+                    </div>
+                </div>
+                <div className="row center-xs">
+                    <div className="col-md-6 col-sm-8 col-xs-10">
+                        <button className="btn btn_blue btn_lg team-create__create" disabled="disabled">Continue to Team</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+function getProps(state) {
+    return {
+        email: "maxim@izst.ru"
+    }
+}
+
+export default connect(getProps)(Step3)
