2d5422935dbe9b13610890112a098e050d3ce62e
max_buranbaev <max@izst.ru>
2016-10-26 16:33:56 +0200

    #2597 route for the second step

--- public/js/team.js
+++ public/js/team.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import PromoPage from './components/promo/PromoPage.jsx'
 import Layout from './components/promo/team/Layout.jsx'
 import Step1 from './components/promo/team/Step1.jsx'
+import Step2 from './components/promo/team/Step2.jsx'
 import store from './store.js'
 
 
@@ -12,9 +13,10 @@ import { Provider } from 'react-redux'
 const router = (
     <Provider store={ store }>
         <Router history={ browserHistory }>
-            <Route path="/promo" component={ PromoPage } />
-            <Route path="/promo/team/" component={ Layout }>
-                <IndexRoute component={ Step1 } />
+            <Route path="/promo" component={ PromoPage }></Route>
+            <Route path="/team" component={ Layout }>
+                <Route path="step1" component={ Step1 } ></Route>
+                <Route path="step2" component={ Step2 } ></Route>
             </Route>
         </Router>
     </Provider>
