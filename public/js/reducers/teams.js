2d1449c88b7334e6dc32af8b12ae425745645166
max_buranbaev <max@izst.ru>
2016-10-26 10:53:51 +0200

    #2596 reducer for the step0

--- public/js/reducers/teams.js
+++ public/js/reducers/teams.js
@@ -3,6 +3,11 @@ export default function(state = {}, action) {
     const { type, payload } = action;
 
     switch (type) {
+
+        case "CREATE_TEAM":
+            return Object.assign({}, state, { email: action.createItem.data.email });
+            break;
+
         default:
             return state;
             break;
