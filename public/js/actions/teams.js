4e7f087b6c85ef54b66cbc086450d62b1f4bf040
max_buranbaev <max@izst.ru>
2016-10-26 10:52:53 +0200

    #2596 function for the step0

--- public/js/actions/teams.js
+++ public/js/actions/teams.js
@@ -1,5 +1,11 @@
-export function step0 {
-    return {
+import { TEAM_CRUD } from '../constants'
 
+export function step0(email) {
+    return {
+        type: "CREATE_TEAM",
+        createItem: {
+            url: TEAM_CRUD,
+            data: { email: email }
+        }
     }
 }
