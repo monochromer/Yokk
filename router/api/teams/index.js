607e0d5486fc9b46834081fa0e3ef43a2ecc3bb7
olegzhermal <olegzhermal@gmail.com>
2016-10-26 13:10:19 +0500

    #2604 API URL changed

--- router/api/teams/index.js
+++ router/api/teams/index.js
@@ -3,7 +3,7 @@ const handle = require('./handlers');
 module.exports = function(api) {
     // api.route('/team/checkConfirmationCode/')
     //     .get(handle.checkConfirmationCode);
-    api.route('/team(/:name)?')
+    api.route('/teams(/:name)?')
         .post(handle.create)
         .get(handle.read)
         .put(handle.update)
