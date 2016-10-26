1e248c3c9877a5cf78712bbb3ba9088a69bc2345
max_buranbaev <max@izst.ru>
2016-10-26 10:53:10 +0200

    #2596 handling data from the form

--- public/js/components/promo/PromoPage.jsx
+++ public/js/components/promo/PromoPage.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import store from '../../store'
+import { step0 } from '../../actions/teams'
+
 
 class PromoPage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email: ""
+        }
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        store.dispatch(step0(this.state.email));
+    }
+
+    handleChange(event) {
+         this.state.email = event.target.value;
+    }
+
     render() {
         return (
             <div className="container-fluid main-container">
@@ -15,26 +34,29 @@ class PromoPage extends React.Component {
                         <h1>Eye of Providence <br /> helps to manage your team</h1>
                     </div>
                     <div className="container team-create">
-                        <div className="row middle-md">
-                            <div
-                                className="col-md-offset-1 col-sm-offset-1 col-xs-offset-1 col-md-7 col-sm-6 col-xs-10">
-                                <input type="text" className="input input__white" placeholder="E-mail address" />
+                        <form onSubmit={ this.handleSubmit.bind(this) }>
+                            <div className="row middle-md">
+                                <div
+                                    className="col-md-offset-1 col-sm-offset-1 col-xs-offset-1 col-md-7 col-sm-6 col-xs-10">
+                                    <input type="email" onChange={ this.handleChange.bind(this) } className="input input__white" placeholder="E-mail address" required />
+                                </div>
+                                <div className="col-md-3 col-sm-4 col-xs-12 hide-xs">
+                                    <button className="btn btn__white btn__lg">Create New Team</button>
+                                </div>
                             </div>
-                            <div className="col-md-3 col-sm-4 col-xs-12 hide-xs">
-                                <button className="btn btn__white btn__lg">Create New Team</button>
+                            <div className="row hide-sm hide-lg hide-md">
+                                <div className="col-xs-12 center-xs">
+                                    <button className="btn btn__white btn__lg team-create__create">Create New Team
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                        <div className="row hide-sm hide-lg hide-md">
-                            <div className="col-xs-12 center-xs">
-                                <button className="btn btn__white btn__lg team-create__create">Create New Team</button>
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
                 <div className="container footer">
                     <div className="span2">
                         <a href="http://soshace.com" target="_blank">
-                            <img src="/img/logo.png" alt="soshace" title="soshace" height="36px" />
+                            <img src="/img/logo.png" alt="soshace" title="soshace" height="36px"/>
                         </a>
                     </div>
                 </div>
