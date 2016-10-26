a24aa6b3b86884519b90f4c3e8735d64d0f24627
max_buranbaev <max@izst.ru>
2016-10-26 13:44:08 +0200

    #2596 adding routes for creating team

--- public/js/team.js
+++ public/js/team.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PromoPage from './components/promo/PromoPage.jsx'
+import Layout from './components/promo/team/Layout.jsx'
+import Step1 from './components/promo/team/Step1.jsx'
 import store from './store.js'
 
 
@@ -11,6 +13,9 @@ const router = (
     <Provider store={ store }>
         <Router history={ browserHistory }>
             <Route path="/promo" component={ PromoPage } />
+            <Route path="/promo/team/" component={ Layout }>
+                <IndexRoute component={ Step1 } />
+            </Route>
         </Router>
     </Provider>
 );
