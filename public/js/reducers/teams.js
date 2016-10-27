f4bbdcdbf3fec93a9d2f1b97c7a4ebb38d72f8ad
max_buranbaev <max@izst.ru>
2016-10-27 13:24:20 +0200

    #2601 reducer for step 2,3,4

--- public/js/reducers/teams.js
+++ public/js/reducers/teams.js
@@ -15,6 +15,21 @@ export default function(state = {}, action) {
             return state;
             break;
 
+        case "STEP_2":
+            browserHistory.push('/team/step3');
+            return Object.assign({}, state, { username: action.username });
+            break;
+
+        case "STEP_3":
+            browserHistory.push('/team/step4');
+            return Object.assign({}, state, { password: action.password });
+            break;
+
+        case "STEP_4":
+            browserHistory.push('/team/step5');
+            return Object.assign({}, state, { teamName: action.teamName });
+            break;
+
         default:
             return state;
             break;
