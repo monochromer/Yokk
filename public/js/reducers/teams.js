00cc25c679dd295848bb4bf1d402df5f757f326f
max_buranbaev <max@izst.ru>
2016-10-27 12:01:41 +0200

    #2601 reducer for step1

--- public/js/reducers/teams.js
+++ public/js/reducers/teams.js
@@ -10,6 +10,11 @@ export default function(state = {}, action) {
             return Object.assign({}, state, { email: action.createItem.data.email });
             break;
 
+        case "STEP_1":
+            browserHistory.push('/team/step2');
+            return state;
+            break;
+
         default:
             return state;
             break;
