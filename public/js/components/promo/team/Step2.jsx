08c003abb176ae77c22e898cad418fd30a99f590
max_buranbaev <max@izst.ru>
2016-10-27 13:23:31 +0200

    #2601 handling forms step2

--- public/js/components/promo/team/Step2.jsx
+++ public/js/components/promo/team/Step2.jsx
@@ -1,43 +1,57 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import store from '../../../store'
+import { step2 } from '../../../actions/teams'
 
 class Step2 extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            username: ""
+        }
+    }
+
+    handleChange(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        store.dispatch(step2(this.state.username));
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="row center-xs step__heading">
-                    <div className="col-md-6 col-sm-8 col-xs-10">
-                        <h1 className="heading">What is your name?</h1>
+            <form onSubmit={ this.handleSubmit.bind(this) }>
+                <div className="container">
+                    <div className="row center-xs step__heading">
+                        <div className="col-md-6 col-sm-8 col-xs-10">
+                            <h1 className="heading">What is your name?</h1>
+                        </div>
+                    </div>
+                    <div className="row center-xs step__message">
+                        <div className="col-md-6 col-sm-8 col-xs-10">
+                            <p>You name will be used for auth. You won't be able to change it.</p>
+                        </div>
+                    </div>
+                    <div className="row center-xs step__code">
+                        <div className="col-md-6 col-sm-8 col-xs-10">
+                            <input className="input input_black" type="text" onChange={ this.handleChange.bind(this) } placeholder="User name"/>
+                        </div>
+                    </div>
+                    <div className="row center-xs">
+                        <div className="col-md-6 col-sm-8 col-xs-10">
+                            <button type="submit" className="btn btn_blue btn_lg team-create__create" disabled={!this.state.username ? "disabled" : "" }>Continue to
+                                Password
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <div className="row center-xs step__message">
-                    <div className="col-md-6 col-sm-8 col-xs-10">
-                        <p>You name will be used for auth. You won't be able to change it.</p>
-                    </div>
-                </div>
-                <div className="row center-xs step__code">
-                    <div className="col-md-6 col-sm-8 col-xs-10">
-                        <input className="input input_black" type="text" placeholder="User name"/>
-                    </div>
-                </div>
-                <div className="row center-xs">
-                    <div className="col-md-6 col-sm-8 col-xs-10">
-                        <button className="btn btn_blue btn_lg team-create__create" disabled="disabled">Continue to Password</button>
-                    </div>
-                </div>
-            </div>
+            </form>
         )
     }
 }
 
-function getProps(state) {
-    return {
-        email: "maxim@izst.ru"
-    }
-}
 
-export default connect(getProps)(Step2)
+export default Step2
