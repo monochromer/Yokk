154527f296fd3c1a6f4b43b0e95b521c5a5586bd
olegzhermal <olegzhermal@gmail.com>
2016-11-01 13:36:01 +0500

    #2621 Fixed bug in report table
    Changing timeEntries model broke logic of getting report data because
    previously data was retrieved by user.login, but now timeEntries
    contain links to user table.

--- router/api/reports/handlers.js
+++ router/api/reports/handlers.js
@@ -1,49 +1,87 @@
 const moment = require('moment');
 const toDate = require('../helpers/toDate').toDate;
+const _ = require('lodash');
 
 exports.createReport = function(req, res, next) {
 
     var query = {};
     var response = {};
-    
+
+    const userModel = req.app.db.models.User;
+    const usersArray = req.query.users.split(',');
+
     const TimeEntryModel = req.app.db.models.timeEntry;
 
-    if(moment(req.query.from, 'DD.MM.YYYY').isValid())
-        query["dateCreated"] = Object.assign({}, query['dateCreated'], { $gte: toDate(req.query.from) });
+    getExecutors(usersArray, userModel).then(users => {
+      return getTheReport(users);
+    }).then(response => {
+      res.status(200).send(response);
+    })
 
-    if(moment(req.query.to, 'DD.MM.YYYY').isValid())
-        query["dateCreated"] = Object.assign({}, query['dateCreated'], { $lte: toDate(req.query.to, 1) });
-
-    query.executor = { $in: req.query.users.split(',') };
-
-    TimeEntryModel.find(query).exec( (err, entries) => {
-        if(err) next(err);
-        let data = {};
-
-        entries.map( (entry) => {
-            var { executor, entrySource, duration } = entry;
-            if(!data[executor]) {
-                data[executor] = {
-                    total: duration,
-                    [entrySource]: duration + 1
-                }
-            } else {
-                data[executor]["total"] += duration;
-
-                if(data[executor][entrySource]) {
-                    data[executor][entrySource] += duration;
-                } else {
-                    data[executor][entrySource] = duration;
-                }
-            }
+    if (moment(req.query.from, 'DD.MM.YYYY').isValid())
+        query["dateCreated"] = Object.assign({}, query['dateCreated'], {
+            $gte: toDate(req.query.from)
         });
 
-        response = {
-            query: query,
-            data: data
-        };
+    if (moment(req.query.to, 'DD.MM.YYYY').isValid())
+        query["dateCreated"] = Object.assign({}, query['dateCreated'], {
+            $lte: toDate(req.query.to, 1)
+        });
 
-        res.status(200).send(response);
-    });
+    function getExecutors(usersArray, userModel) {
+        return new Promise((resolve, reject) => {
+            userModel.find({
+                login: {
+                    $in: usersArray
+                }
+            }, {
+                _id: 1,
+                login: 1
+            }, (err, users) => {
+                resolve(users);
+            })
+        })
+    }
+
+    function getTheReport(users) {
+        query.executor = {$in: users};
+        return new Promise((resolve, reject) => {
+            TimeEntryModel.find(query).exec((err, entries) => {
+                if (err) next(err);
+                let data = {};
+
+                entries.map((entry) => {
+                    var {
+                        entrySource,
+                        duration
+                    } = entry;
+
+                    executor = _.find( users, user => user._id.toString() === entry.executor.toString() ).login;
+                    if (!data[executor]) {
+                        data[executor] = {
+                            total: duration,
+                            [entrySource]: duration + 1
+                        }
+                    } else {
+                        data[executor]["total"] += duration;
+
+                        if (data[executor][entrySource]) {
+                            data[executor][entrySource] += duration;
+                        } else {
+                            data[executor][entrySource] = duration;
+                        }
+                    }
+                });
+
+                response = {
+                    query: query,
+                    data: data
+                };
+
+                resolve(response);
+            });
+        })
+    }
+
 
 };
