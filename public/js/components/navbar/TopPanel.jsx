ddc27393677a81f40ae7a54e6a9828cb98cf7aab
Max Buranbaev <max@izst.ru>
2016-11-03 19:47:34 +0100

    #2625 removing bootstrap

--- public/js/components/navbar/TopPanel.jsx
+++ public/js/components/navbar/TopPanel.jsx
@@ -1,47 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router'
-import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap'
 import { connect } from 'react-redux'
-import { Navbar, Nav, NavDropdown, MenuItem, NavItem } from 'react-bootstrap'
-import { findUserByLogin } from '../../helpers'
 
-var TopPanel = React.createClass({
-	render: function() {
-		return (
-			<Navbar fluid>
-				<Navbar.Header>
-					<Navbar.Brand>
-						<Link className="navbar-brand" to="/">Eye of providence</Link>
-					</Navbar.Brand>
-				</Navbar.Header>
-				<Nav>
-					<IndexLinkContainer to="/">
-						<NavItem eventKey={1}>Tracking</NavItem>
-					</IndexLinkContainer>
-					<LinkContainer to="/users">
-						<NavItem eventKey={2}>Team</NavItem>
-					</LinkContainer>
-					<LinkContainer to="/ReportsPage">
-						<NavItem eventKey={3}>Reports</NavItem>
-					</LinkContainer>
-				</Nav>
-				<Nav pullRight>
-					<NavDropdown eventKey={4} title={ this.props.login } id="basic-nav-dropdown" >
-						<LinkContainer to={ "/user/edit/" + this.props.login }>
-							<MenuItem eventKey={4.1}>Your Profile</MenuItem>
-						</LinkContainer>
-						<MenuItem href="/logout" eventKey={4.2}>Logout</MenuItem>
-					</NavDropdown>
-				</Nav>
-			</Navbar>
-		)
-	}
-});
+class TopPanel extends React.Component {
+    render() {
+        return (
+            <div className="top-panel">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4"><a href="" className="top-panel_logo">Eye of Providence</a></div>
+                        <div className="col-md-4">
+                            <ul>
+                                <li><Link to="/team">Team</Link></li>
+                                <li><Link to="/reports">Reports</Link></li>
+                            </ul>
+                        </div>
+                        <div className="col md-2">
+                            <Link to="/tracking">Tracking</Link>
+                        </div>
+                        <div className="col-md-2">
+                            <span>Profile</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
-var getProps = function(store) {
-	return {
-		login: store.currentUser.login
-	}
+const getProps = function (store) {
+    return {
+        login: store.currentUser.login
+    }
 };
 
 export default connect(getProps)(TopPanel);
