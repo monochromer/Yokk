8912a80e7d9c300ac4afd5ee84cd8b26a97aaf28
Max Buranbaev <max@izst.ru>
2016-11-03 19:47:09 +0100

    #2625 refactoring

--- public/js/components/Layout.jsx
+++ public/js/components/Layout.jsx
@@ -3,15 +3,12 @@ import Alert from './alert/Alert.jsx'
 import ModalUserDelete from './user/ModalUserDelete.jsx'
 import TopPanel from './navbar/TopPanel.jsx'
 import store from '../store.js'
-
-// import { fetchUsers } from '../actions/users.js'
 import { fetchTeamUsers } from '../actions/users.js'
 import { fetchCurrentUser } from '../actions/currentUser.js'
 
 class Layout extends React.Component {
 
     componentWillMount() {
-        // store.dispatch(fetchUsers());
         store.dispatch(fetchTeamUsers());
         store.dispatch(fetchCurrentUser());
     }
@@ -22,7 +19,8 @@ class Layout extends React.Component {
                 <TopPanel location={this.props.location.pathname}/>
                 <div className="container-fluid">
                     <Alert/>
-                    <ModalUserDelete/> { this.props.children }
+                    <ModalUserDelete/>
+                    { this.props.children }
                 </div>
             </div>
         );
