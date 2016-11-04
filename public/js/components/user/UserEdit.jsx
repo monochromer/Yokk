44034fb265e4bdd7b6b2c91b2d0b02a7773e2bf0
Max Buranbaev <max@izst.ru>
2016-11-04 15:40:30 +0100

    #2626 implementing new design

--- public/js/components/user/UserEdit.jsx
+++ public/js/components/user/UserEdit.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import _ from 'lodash'
 import store from '../../store.js'
 import DropPicture from './DropPicture.jsx'
 import ModalChangePassword from './ModalChangePassword.jsx'
@@ -8,172 +7,147 @@ import { updateUser } from '../../actions/users'
 import { browserHistory } from 'react-router'
 import { connect } from 'react-redux'
 
-var UserEdit = React.createClass({
+class UserEdit extends React.Component {
 
-    getInitialState: function( ) {
-      console.log(this.props)
-        return {
-            user: findUserByLogin( this.props.users, this.props.routeParams.login )
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: findUserByLogin(this.props.users, this.props.routeParams.login)
         }
-    },
+    }
 
-    componentWillReceiveProps: function( nextProps ) {
+    componentWillReceiveProps(nextProps) {
         this.setState({
-            user: findUserByLogin( nextProps.users, this.props.routeParams.login )
+            user: findUserByLogin(nextProps.users, this.props.routeParams.login)
         })
-    },
+    }
 
-    handleSubmit: function( event ) {
+    handleSubmit(event) {
+        let fields = {};
         event.preventDefault();
-        var fields = {};
 
-        for ( var field in this.refs ) {
+        for (let field in this.refs) {
             fields[field] = this.refs[field].value;
         }
 
         fields.login = this.state.user.login;
-        store.dispatch(updateUser( fields ));
+        store.dispatch(updateUser(fields));
         browserHistory.push('/user/' + fields.login);
-    },
+    }
 
-    openModalChangePassword: function() {
-        store.dispatch({type: "MODAL_CHANGE_PASSWORD_SHOW", login: this.state.user.login});
-    },
+    openModalChangePassword() {
+        store.dispatch({ type: "MODAL_CHANGE_PASSWORD_SHOW", login: this.state.user.login });
+    }
 
-    render: function( ) {
-        if ( this.state.user == undefined ) {
+    render() {
+
+        if (this.state.user) {
+            const { profileImg, login, fullname, position, phone, skype, workhours, email, birthday, vk, aboutme, cv } = this.state.user;
+            const photo = profileImg ? profileImg.medium : "";
             return (
-                <p> Wait a moment please... </p>
-            );
-        } else {
-            const photo = this.state.user.profileImg ? this.state.user.profileImg.original : "";
-            return (
-                <div className="container-fluid">
+                <div className="container profile">
                     <div className="row">
-                        <div className="col-md-3 text-center center-block profile__photo">
-                            <DropPicture login={this.state.user.login} photo={photo}/>
+
+                        <div className="col-md-3 profile_photo">
+                            <DropPicture login={ login } photo={ photo }/>
                         </div>
-                        <div className="col-md-9 profile">
-                            <form onSubmit={this.handleSubmit}>
-                                <h2>{this.state.user.login}</h2>
-                                <button type="button" className="btn btn-default" onClick={ this.openModalChangePassword }>
-                                    Change Password
-                                </button>
-                                <div className="row">
-                                    <div className="col-md-10">
-                                        <h3>General</h3>
+
+                        <div className="col-md-9">
+                            <form onSubmit={ this.handleSubmit }>
+                                <h2>{ login }</h2>
+                                <div className="row profile_section">
+                                    <div className="col-md-12">
+                                        <h3 className="profile_heading">General</h3>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="fullname">Full Name</label>
-                                            <input type="text" className="form-control" ref="fullname" id="fullname" placeholder="Jack Shephard" defaultValue={this.state.user.fullname}/>
-                                        </div>
+                                        <input type="text" className="input input__grey"
+                                               placeholder="FullName" defaultValue={ fullname }/>
                                     </div>
                                     <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="position">Position</label>
-                                            <input type="text" className="form-control" ref="position" id="position" placeholder="Developer" defaultValue={this.state.user.position}/>
-                                        </div>
+                                        <input type="text" className="input input__grey"
+                                               placeholder="Developer" defaultValue={ position }/>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="phone-number">Phone Number</label>
-                                            <input type="text" className="form-control" ref="phone" id="phone-number" placeholder="+4 815 16 23 42" defaultValue={this.state.user.phone}/>
-                                        </div>
+
+                                        <input type="text" className="input input__grey"
+                                               placeholder="+4 815 16 23 42" defaultValue={ phone }/>
+
                                     </div>
                                     <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="skype">Skype</label>
-                                            <input type="text" className="form-control" ref="skype" id="skype" placeholder="jackshephard" defaultValue={this.state.user.skype}/>
-                                        </div>
+                                        <input type="text" className="input input__grey"
+                                               placeholder="jackshephard" defaultValue={ skype }/>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="workhours">Work Hours</label>
-                                            <input type="text" className="form-control" ref="workhours" id="workhours" placeholder="07:00 - 17:00" defaultValue={this.state.user.workhours}/>
-                                        </div>
+                                        <input type="text" className="input input__grey" placeholder="07:00 - 17:00" defaultValue={ workhours }/>
                                     </div>
                                     <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="email">Email</label>
-                                            <input type="email" className="form-control" ref="email" id="email" placeholder="dr.shepard@gmail.com" defaultValue={this.state.user.email}/>
-                                        </div>
+                                            <input type="email" className="input input__grey"
+                                                   placeholder="dr.shepard@gmail.com"
+                                                   defaultValue={ this.state.user.email }/>
+                                    </div>
+                                </div>
+                                <div className="row profile_section">
+                                    <div className="col-md-12">
+                                        <h3 className="profile_heading">Personal</h3>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <input type="text" className="input input__grey"
+                                               placeholder="01.11.1990" defaultValue={ this.state.user.birthday }/>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <input type="text" className="input input__grey"
+                                               placeholder="vk.com/jackshephard" defaultValue={ this.state.user.vk }/>
+                                    </div>
+                                </div>
+                                <div className="row profile_section">
+                                    <div className="col-md-12">
+                                        <h3 className="profile_heading">About me</h3>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-12">
-                                        <h3>Personal</h3>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="birthday">Birthday</label>
-                                            <input type="text" className="form-control" ref="birthday" id="birthday" placeholder="01.11.1990" defaultValue={this.state.user.birthday}/>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-6">
-                                        <div className="form-group">
-                                            <label htmlFor="vk">VK</label>
-                                            <input type="text" className="form-control" ref="vk" id="vk" placeholder="vk.com/jackshephard" defaultValue={this.state.user.vk}/>
-                                        </div>
+                                        <textarea className="input input__grey" name="aboutme" rows="5"
+                                                  defaultValue={ this.state.user.aboutme }></textarea>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-12">
-                                        <h3>About me</h3>
+                                        <input type="text" className="input input__grey"
+                                               defaultValue={this.state.user.cv}/>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-12">
-                                        <div className="form-group">
-                                            <label htmlFor="aboutme">Some text about you</label>
-                                            <textarea className="form-control" ref="aboutme" id="aboutme" rows="5" defaultValue={this.state.user.aboutme}></textarea>
-                                        </div>
+                                        <input type="text" className="input input__grey"
+                                               defaultValue={this.state.user.redmineApiKey}/>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="col-md-12">
-                                        <div className="form-group">
-                                            <label htmlFor="cv">Link to your CV (hh.ru or other)</label>
-                                            <input type="text" className="form-control" ref="cv" defaultValue={this.state.user.cv}/>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <div className="form-group">
-                                            <label htmlFor="redmineApiKey">Redmine API key</label>
-                                            <input type="text" className="form-control" id="redmineApiKey" ref="redmineApiKey" defaultValue={this.state.user.redmineApiKey} />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <button type="submit" className="btn btn-lg btn-success" style={{
-                                            "margin": "20px 0",
-                                            "float": "right"
-                                        }}>Save</button>
+                                        <button type="submit" className="btn">Save</button>
                                     </div>
                                 </div>
                             </form>
                         </div>
                     </div>
-                    <ModalChangePassword login={this.props.routeParams.login}/>
                 </div>
             )
+        } else {
+            return ( <p> Wait a moment please... </p>);
         }
     }
-})
+}
 
-var fetchUserStateToProps = function( state ) {
+function fetchUserStateToProps(state) {
     return { users: state.users }
 }
 
-export default connect( fetchUserStateToProps )( UserEdit );
+export default connect(fetchUserStateToProps)(UserEdit);
