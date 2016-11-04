ed1ed47ef0b8fd476a7f4712847ca55f95e07429
Max Buranbaev <max@izst.ru>
2016-11-04 05:11:48 +0100

    #2625 html2psd top panel

--- public/js/components/navbar/TopPanel.jsx
+++ public/js/components/navbar/TopPanel.jsx
@@ -6,20 +6,39 @@ class TopPanel extends React.Component {
     render() {
         return (
             <div className="top-panel">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-4"><a href="" className="top-panel_logo">Eye of Providence</a></div>
+                <div className="container top-panel_container">
+                    <div className="row top-panel_row">
                         <div className="col-md-4">
-                            <ul>
-                                <li><Link to="/team">Team</Link></li>
-                                <li><Link to="/reports">Reports</Link></li>
+                            <a href="" className="top-panel_logo">Eye of Providence</a>
+                        </div>
+                        <div className="col-md-4">
+                            <ul className="top-panel_menu">
+                                <li className="top-panel_menu-item">
+                                    <Link className="top-panel_menu-link" activeClassName="top-panel_menu-link__active"
+                                          to="/users">Team</Link>
+                                </li>
+                                <li className="top-panel_menu-item">
+                                    <Link className="top-panel_menu-link" activeClassName="top-panel_menu-link__active"
+                                          to="/reports">Reports</Link>
+                                </li>
                             </ul>
                         </div>
-                        <div className="col md-2">
-                            <Link to="/tracking">Tracking</Link>
+                        <div className="col-md-2 col-md-offset-1 tracking-item">
+                            <Link className="tracking-block_link" to="/tracking">
+                                <div className="tracking-block">
+                                    <span>Tracking</span>
+                                </div>
+                            </Link>
                         </div>
-                        <div className="col-md-2">
-                            <span>Profile</span>
+                        <div className="col-md-1">
+                            <div className="profile-block">
+                                <img src="/img/max@3x.png" alt="maxim"/>
+                                <div className="dropdown">
+                                    <div className="dropdown-title ">
+                                        { this.props.login }
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
