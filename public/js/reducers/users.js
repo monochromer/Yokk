42b657c280614a150a18ea637630600356486b62
Max Buranbaev <max@izst.ru>
2016-11-07 14:54:36 +0100

    #2626 refactoring & new field

--- public/js/reducers/users.js
+++ public/js/reducers/users.js
@@ -1,38 +1,44 @@
 import _ from 'lodash'
 
-const defaultState = [];
+const defaultState = {
+    list: [],
+    status: "ready"
+};
 
 export default function(state = defaultState, action) {
-    const {
-        type,
-        payload
-    } = action;
+    const { type, payload } = action;
 
     switch (type) {
         case "ADD_USER":
-            return [...state, payload];
+            return Object.assign({}, state, { list: [...state.list, payload] });
             break;
 
         case "DELETE_USER":
-            return _.filter(state, (o) => o.login != payload);
+            return Object.assign({}, state, { list: [...state.list, _.filter(state, (o) => o.login != payload)] });
             break;
 
-        case "UPDATE_USER_SUCCESFULL":
-            const newState = _.filter(state, (o) => o.login != payload.login);
-            return [...newState, payload];
+        case "SET_READY_STATE":
+            return Object.assign({}, state, { status: "ready" });
+            break;
+
+        case "UPDATE_USER_SUCCESS":
+            const newState = _.filter(state.list, (o) => o._id != payload._id);
+            return Object.assign({}, state, {
+                status: "success",
+                list: [...newState, payload]
+            });
             break;
 
         case "UPDATE_USER_START":
-            const newState = _.filter(state, (o) => o.login != payload.login);
-            return [...newState, payload];
+            return Object.assign({}, state, { status: "loading" });
             break;
 
         case "FETCH_USERS":
-            return payload;
+            return Object.assign({}, state, { list: payload });
             break;
 
         case "FETCH_TEAM_USERS":
-            return payload;
+            return Object.assign({}, state, { list: payload });
             break;
 
         default:
