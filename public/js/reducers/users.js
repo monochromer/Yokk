a22af0b489de013a6e15d31aa02ec42ca442e47f
Max Buranbaev <max@izst.ru>
2016-11-08 09:36:59 +0100

    #2625 actions for linking modal

--- public/js/reducers/users.js
+++ public/js/reducers/users.js
@@ -1,13 +1,17 @@
 import _ from 'lodash'
 
+var linkService = {
+    status: "hidden"
+};
+
 const defaultState = {
     list: [],
-    status: "ready"
+    status: "ready",
+    linkService: linkService
 };
 
 export default function(state = defaultState, action) {
     const { type, payload } = action;
-
     switch (type) {
         case "ADD_USER":
             return Object.assign({}, state, { list: [...state.list, payload] });
@@ -39,6 +43,33 @@ export default function(state = defaultState, action) {
 
         case "FETCH_TEAM_USERS":
             return Object.assign({}, state, { list: payload });
+            break;
+
+        case "LINK_SERVICE":
+            linkService = {
+                status: 'ready',
+                service: action.service,
+                userId: action.userId
+            };
+            return Object.assign({}, state, { linkService: linkService });
+            break;
+
+        case "LINK_SERVICE_START":
+            linkService = { status: 'loading' };
+            return Object.assign({}, state, { linkService: linkService });
+            break;
+
+        case "LINK_SERVICE_SUCCESS":
+            linkService = { status: 'success' };
+            const newList = _.filter(state.list, (o) => o._id != payload._id);
+            return Object.assign({}, state, {
+                linkService: linkService,
+                list: [...newList, payload]
+            });
+
+        case "LINK_SERVICE_CLOSE":
+            linkService = { status: 'hidden' };
+            return Object.assign({}, state, { linkService: linkService });
             break;
 
         default:
