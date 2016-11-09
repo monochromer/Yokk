40948da970773a45d2bbea76b62ff1123c4d34ab
Max Buranbaev <max@izst.ru>
2016-11-09 12:20:26 +0100

    #2629 grid instead tables

--- public/js/components/tracking/TimeEntriesPerDay.jsx
+++ public/js/components/tracking/TimeEntriesPerDay.jsx
@@ -8,39 +8,34 @@ class TimeEntriesPerDay extends React.Component {
     }
 
     render() {
-        var rows = [];
+        let rows = [];
         this.props.timeEntries.map( (timeEntry) => {
             rows.push(<TimeEntryRow timeEntry={ timeEntry } key={ timeEntry._id } />);
         });
 
         return (
             <div className="row">
-                <div className="col-md-12">
-                    <div className="row day">
-                        <div className="col-md-4 day__date">
-                            <h3>{ this.props.day } &nbsp;
-                                <span className="day__total-duration">
-                                    { this.props.duration }</span>
+                <div className="col-md-12 tracking-day">
+                    <div className="row tracking-day_row">
+                        <div className="col-md-1"></div>
+                        <div className="col-md-4">
+                            <h3>
+                                <span>
+                                    { this.props.day }
+                                </span>
+                                <span className="tracking-day_duration">
+                                    { this.props.duration }
+                                </span>
                             </h3>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-12">
-                            <table className="table table-striped timeEntries">
-                                <thead>
-                                    <tr>
-                                        <th className="timeEntries__head issue__source"></th>
-                                        <th className="timeEntries__head issue__description">Task</th>
-                                        <th className="timeEntries__head issue__duration">Duration</th>
-                                        <th className="timeEntries__head issue__actions">Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    { rows }
-                                </tbody>
-                            </table>
-                        </div>
+                    <div className="row tracking-table tracking-table_head">
+                        <div className="col-md-1"></div>
+                        <div className="col-md-9 tracking-table_heading">Description</div>
+                        <div className="col-md-1 tracking-table_heading">Time</div>
+                        <div className="col-md-1 tracking-table_heading"></div>
                     </div>
+                    { rows }
                 </div>
             </div>
         )
