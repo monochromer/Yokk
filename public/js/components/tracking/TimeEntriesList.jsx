1392ac2ef1fc5ffdbdee265f5cc9f09fa75d9efc
Max Buranbaev <max@izst.ru>
2016-11-09 12:19:58 +0100

    #2629 PSD2HTML, using classNames

--- public/js/components/tracking/TimeEntriesList.jsx
+++ public/js/components/tracking/TimeEntriesList.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { browserHistory } from 'react-router'
 import TimeEntriesPerDay from './TimeEntriesPerDay.jsx'
 import store from '../../store'
-import moment from 'moment'
-import {fetchNextTimeEntryBatch} from '../../actions/timeEntries.js'
-import {connect} from 'react-redux'
-import {dayBeatify, durationBeatify, groupTimeEntriesByDay} from '../../helpers'
+import classNames from 'classnames'
+import { fetchNextTimeEntryBatch } from '../../actions/timeEntries.js'
+import { connect } from 'react-redux'
+import { dayBeatify, durationBeatify, groupTimeEntriesByDay } from '../../helpers'
 
 class TimeEntriesList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { limit: 10 }
+        this.state = { limit: 10 };
+        this.loadMore = this.loadMore.bind(this);
     }
 
     componentWillMount() {
@@ -24,23 +25,33 @@ class TimeEntriesList extends React.Component {
     }
 
     render() {
-        const {days} = this.props;
-        var rows = [];
-        for (var day in days) {
-            var duration = durationBeatify(days[day].totalDuration);
-            rows.push(<TimeEntriesPerDay day={day} duration={duration} timeEntries={days[day].list} key={day}/>)
+        const { days } = this.props;
+
+        let rows = [];
+        for (let day in days) {
+            let duration = durationBeatify(days[day].totalDuration);
+            rows.push(<TimeEntriesPerDay day={ day } duration={ duration } timeEntries={ days[day].list } key={ day }/>)
         }
-        const loadMoreClasses = this.props.allBatches ? 'btn btn-success center-block loadmore disabled' : 'btn btn-success center-block loadmore' ;
+
+        const loadMoreClasses = classNames({
+            'btn btn__md btn__trans-blue loadmore' : true,
+            'btn__disabled': this.props.allBatches
+        });
+        const disabled = this.props.allBatches ? 'disabled' : "";
         return (
             <div className="container container__fixed">
-                {rows}
-                <button className={loadMoreClasses} onClick={this.loadMore}>
-                    Load More
-                </button>
+                { rows }
+                <div className="row center-md">
+                    <div className="col-md-4 text-center">
+                        <button className={ loadMoreClasses } onClick={this.loadMore} disabled={ disabled }> Load More </button>
+                    </div>
+                </div>
+
             </div>
         )
     }
-};
+}
+
 
 function getProps(state) {
     return {
