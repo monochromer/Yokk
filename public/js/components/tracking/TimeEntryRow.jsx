42bcf6c3235bb7198411c5f1d1d2d27522b86400
Max Buranbaev <max@izst.ru>
2016-11-09 12:20:40 +0100

    #2629 handling basic actions

--- public/js/components/tracking/TimeEntryRow.jsx
+++ public/js/components/tracking/TimeEntryRow.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
-import InputElement from 'react-input-mask'
 import moment from 'moment'
 import store from '../../store'
 import { deleteTimeEntry, updateTimeEntry } from '../../actions/timeEntries.js'
 import { durationBeatify, refsToObject } from '../../helpers'
+import { Input } from '../UI.jsx'
 
 
 class TimeEntryRow extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { editing: false }
+
+        this.state = {
+            editing: false,
+            fields: {}
+        };
+
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+        this.handleSave = this.handleSave.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    handleDelete(e) {
+    handleDelete() {
         store.dispatch(deleteTimeEntry(this.props.timeEntry._id));
     }
 
@@ -24,67 +34,83 @@ class TimeEntryRow extends React.Component {
         this.setState({ editing: false });
     }
 
-    handleSave(e) {
-        e.preventDefault();
-        let editedTimeEntry = Object.assign({}, this.props.timeEntry, refsToObject(this.refs));
+    handleSave(event) {
+        event.preventDefault();
+        let editedTimeEntry = Object.assign({}, this.props.timeEntry, this.state.fields);
         editedTimeEntry.duration = moment.duration(editedTimeEntry.duration).asMinutes() || this.props.timeEntry.duration;
-        console.log(editedTimeEntry);
         store.dispatch(updateTimeEntry(editedTimeEntry));
         this.setState({ editing: false });
+    }
+
+    handleChange(event) {
+        this.state = {
+            fields: {
+                [event.target.name]: event.target.value
+            }
+        }
     }
 
     render() {
 
         let { entrySource, description, duration, number } = this.props.timeEntry;
+        duration = durationBeatify(duration, 'short');
+
         if (entrySource == "redmine") {
-            description = '<a href="http://redmine.soshace.com/issues/' + number + '">issue ' + number + '</a> ' + description;
+            let link = `http://redmine.soshace.com/issues/number${number}`;
+
+            description = (
+                <span>
+                    <a href={ link }>issue { number }</a> &nbsp;
+                    { description }
+                </span>
+            )
+        } else {
+            if(this.state.editing) {
+                description = (
+                    <Input handleChange={ this.handleChange }
+                           defaultValue={ description }
+                           name="description"
+                           label=""
+                           className="input-group input-group__grey"/>
+                );
+
+                duration = (
+                    <Input handleChange={ this.handleChange }
+                           defaultValue={ duration }
+                           name="duration"
+                           label=""
+                           className="input-group input-group__grey"/>
+                )
+            }
         }
 
         let buttons = (
             <div>
-                <button type="button" className="btn btn-default btn-xs issue__leftButton" onClick={ this.handleEdit }>
-                    <span className="glyphicon glyphicon-edit" aria-hidden="true"></span>
-                </button>
-                <button type="button" className="btn btn-default btn-xs" onClick={ this.handleDelete }>
-                    <span className="glyphicon glyphicon-trash" aria-hidden="true"></span>
-                </button>
+                <button type="button" className="btn btn__icon btn__edit" onClick={ this.handleEdit }></button>
+                <button type="button" className="btn btn__icon btn__delete" onClick={ this.handleDelete }></button>
             </div>
         );
+
         let buttonsEditing = (
             <div>
-                <button type="button" className="btn btn-default btn-xs issue__leftButton" onClick={ this.handleSave }>
-                    <span className="glyphicon glyphicon-ok" aria-hidden="true"></span>
-                </button>
-                <button type="button" className="btn btn-default btn-xs" onClick={ this.handleCancel }>
-                    <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
-                </button>
+                <button type="button" className="btn btn__icon btn__save" onClick={ this.handleSave }></button>
+                <button type="button" className="btn btn__icon btn__cancel" onClick={ this.handleCancel }></button>
             </div>
         );
+
         let buttonsBlock = "";
         if (entrySource != "redmine") {
             buttonsBlock = this.state.editing ? buttonsEditing : buttons;
         }
-        const sourceIcon = entrySource == "redmine" ? '<span><img src="/img/redmine_fluid_icon.png" width="20px"/></span>' : '';
+
+        const sourceIcon = entrySource == "redmine" ? (<img src="/img/redmine-active.svg" width="40px"/>) : '';
         return (
-            <tr>
-                <td dangerouslySetInnerHTML={{ __html: sourceIcon }}></td>
-                <td>
-                    {
-                        !this.state.editing
-                            ? <span dangerouslySetInnerHTML={{ __html: description }}></span>
-                            : <input className="form-control" defaultValue={ description } ref="description"/>
-                    }
-                </td>
-                <td>
-                    {
-                        !this.state.editing
-                            ? durationBeatify(duration)
-                            : <InputElement className="form-control" mask="9:99"
-                                            defaultValue={ durationBeatify(duration) } ref="duration"/>
-                    }
-                </td>
-                <td> { buttonsBlock } </td>
-            </tr>
+            <div className="row entry-row vertical-center">
+                <div className="col-md-1 text-center">{ sourceIcon }</div>
+                <div className="col-md-9 entry-row_description">{ description }</div>
+                <div className="col-md-1">{ duration }</div>
+                <div className="col-md-1"> { buttonsBlock } </div>
+            </div>
         )
     }
 }
