46c283c452166dd26d0590d3324001627aafd618
Yahor Zhykh <egor990095@gmail.com>
2017-02-20 13:02:07 +0300

    #2814 Adjusted file_upload function

--- helpers/file_upload.js
+++ helpers/file_upload.js
@@ -3,20 +3,21 @@
 const multer = require('multer');
 const moment = require('moment');
 const fs = require('fs');
-const path = require('path')
-const baseDir = path.join(__dirname, '../uploads/users/');
+const path = require('path');
+
+const uploadDir = path.join(__dirname, '../uploads');
+const baseDir = path.join(uploadDir, 'users');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        var userDir = baseDir + req.params.user_login;
-        var avatarDir = `${userDir}/avatars/`;
-        if (!fs.existsSync(userDir)) {
-            fs.mkdirSync(userDir);
-            fs.mkdirSync(avatarDir);
-        }
-        if (!fs.existsSync(avatarDir)) {
-            fs.mkdirSync(avatarDir);
-        }
+        const userDir = path.join(baseDir, req.params.user_login);
+        const avatarDir = path.join(userDir, 'avatars', '/');
+
+        if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+        if (!fs.existsSync(baseDir)) fs.mkdirSync(baseDir);
+        if (!fs.existsSync(userDir)) fs.mkdirSync(userDir);
+        if (!fs.existsSync(avatarDir)) fs.mkdirSync(avatarDir);
+
         cb(null, avatarDir);
     },
     filename: function(req, file, cb) {
