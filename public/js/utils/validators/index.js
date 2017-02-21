ccd0cb46259aeae29553b44e511fa8a03f02396b
Yahor Zhykh <egor990095@gmail.com>
2017-02-21 15:55:03 +0300

    Added form validation and error displaying

--- public/js/utils/validators/index.js
+++ public/js/utils/validators/index.js
@@ -4,3 +4,11 @@ export function validateDuration(duration) {
     duration = moment.duration(duration);
     return (moment.isDuration(duration) && (duration.asMinutes() > 0));
 }
+
+export function validateString(str) {
+    return typeof str === 'string' && str.length > 0;
+}
+
+export function validateDateFormat(date) {
+    return /^\d{2}\.\d{2}\.\d{4}$/.test(date);
+}
