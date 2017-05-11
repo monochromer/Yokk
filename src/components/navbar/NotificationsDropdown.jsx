39b50cfc991aca18fc6d253b856d314eee3e35ae
mozjiqok <moskxxi@gmail.com>
2017-05-11 23:51:29 +1000

    #3597 Added elements to notifications dropdown

--- src/components/navbar/NotificationsDropdown.jsx
+++ src/components/navbar/NotificationsDropdown.jsx
@@ -7,11 +7,22 @@ class UserMenu extends React.Component {
   render() {
     const { notifications } = this.props;
 
+    let mappedNotifications = [];
+    for(let i = 0; i < notifications.length && i < 5; i++){
+      const el = notifications[notifications.length - 1 - i];
+      mappedNotifications.push(
+        <div key={el._id} className={"dropdown-element " + (el.new ? 'new' : '')}>
+          <div className="new-notification-circle"></div>
+          <Link to={el.link}>{el.text}</Link>
+        </div>
+      );
+    }
+
     return (
       <div className="dropdown">
-        <div>notifications</div>
-        <div>
-          <a>Logout</a>
+        {mappedNotifications}
+        <div className="text-center">
+          <Link to="/notifications">&gt;&gt; View All &lt;&lt;</Link>
         </div>
       </div>
     )
