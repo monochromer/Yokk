d5b5e74f06bf59b86b5d129fbae7846831fa03c3
Roman Dynkin <roman.dynkin@gmail.com>
2017-05-12 22:46:06 +0300

    Left menu for Settings page. #3581

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,15 +1,37 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchTEams } from '../../actions/teams.js';
+import SettingsLeftMenu from './SettingsLeftMenu.jsx';
 
+/**
+ * Component for user and companies settings
+ */
 class Settings extends Component {
+
+  /**
+   * Constructor
+   * @param props
+   */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      settingsActiveSection: 'user-profile'
+    };
+
+    this.onSettingsSectionChange = this.onSettingsSectionChange.bind(this);
+  }
 
   static PropTypes = {
     user: PropTypes.object,
     teams: PropTypes.array
   }
 
+  /**
+   * React componentWillReceiveProps
+   * @param nextProps
+   */
   componentWillReceiveProps(nextProps) {
     const { companyId } = nextProps.user;
     if (companyId.length && this.props.user.companyId !== companyId){
@@ -17,6 +39,9 @@ class Settings extends Component {
     }
   }
 
+  /**
+   * React componentWillMount
+   */
   componentWillMount() {
     const { companyId } = this.props.user;
     if (companyId.length) {
@@ -24,20 +49,50 @@ class Settings extends Component {
     }
   }
 
+  /**
+   * Handler for change settings section
+   * @param e
+   */
+  onSettingsSectionChange(e) {
+    this.setState({
+      settingsActiveSection: e.target.getAttribute("data-settings-section")
+    });
+  }
+
+  /**
+   * React Render
+   * @return {XML}
+   */
   render() {
     const {user, teams} = this.props;
-    console.log(user);
-    console.log(teams);
 
     return (
       <div className="container container__flex1 container__fixed">
-        <h3>Settings</h3>
+        <div className="row">
+          <div className="col-xs-12 col-sm-4 col-md-3">
+            <SettingsLeftMenu
+              settingsActiveSection={this.state.settingsActiveSection}
+              onSettingsSectionChange={this.onSettingsSectionChange}
+              user={user}
+            />
+          </div>
+          <div className="col-xs-12 col-sm-8 col-md-9">
+            <div className="settings__content">
+              {this.state.settingsActiveSection}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 
 }
 
+/**
+ * Redux get parameters
+ * @param state
+ * @return {{user: {Object}, teams: {Array}}}
+ */
 function getParams(state) {
   return {
     user: state.currentUser.data,
