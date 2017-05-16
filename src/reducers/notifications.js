a07c88d793876ba16b9b9d7c43fc71d22c86e760
mozjiqok <moskxxi@gmail.com>
2017-05-16 23:02:25 +1000

    #3607 Added types to notification reducer

--- src/reducers/notifications.js
+++ src/reducers/notifications.js
@@ -1,52 +1,40 @@
-var initialState = [
-  {
-    _id: 'a1',
-    text: 'a1',
-    new: false,
-    link: '/teams',
-    date: Date.now()
-  },
-  {
-    _id: 'a2',
-    text: 'a2',
-    new: false,
-    link: '',
-    date: Date.now()
-  },
-  {
-    _id: 'a3',
-    text: 'a3',
-    new: false,
-    link: '',
-    date: Date.now()
-  },
-  {
-    _id: 'a4',
-    text: 'a4loooooooooong teeeeeext textexte text',
-    new: false,
-    link: '',
-    date: Date.now()
-  },
-  {
-    _id: 'a5',
-    text: 'a5',
-    new: true,
-    link: '',
-    date: Date.now()
-  },
-  {
-    _id: 'a6',
-    text: 'a6',
-    new: true,
-    link: '',
-    date: Date.now()
-  }
-];
+import {
+  FETCH_NOTIFICATIONS,
+  MARK_NOTIFICATION,
+  MARK_ALL_NOTIFICATIONS
+} from '../constants';
+var initialState = [];
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case "FETCH_NOTIFICATIONS":
-      return action.payload;
+    case FETCH_NOTIFICATIONS:
+      return action.notifications;
+    case MARK_NOTIFICATION:
+      let index = -1;
+      for(let i = 0; i < state.length; i++){
+        if (state[i]._id === action._id) {
+          index = i;
+          break;
+        }
+      }
+      if(index > -1){
+        const notification = state[index];
+        notification.new = false;
+        return [
+          ...state.slice(0, index),
+          notification,
+          ...state.slice(index + 1)
+        ];
+      }
+      else{
+        return state;
+      }
+    case MARK_ALL_NOTIFICATIONS:
+      const newState = state.map((el) => {
+        el.new = false;
+        return el;
+      });
+      return newState;
 
     default:
       return state;
