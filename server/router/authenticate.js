0b5a21fe20800f7efffb48f6c52cc28b7a1e272e
mozjiqok <moskxxi@gmail.com>
2017-05-20 00:09:33 +1000

    Added lean method to finding a user action

--- server/router/authenticate.js
+++ server/router/authenticate.js
@@ -3,23 +3,23 @@ import jwt from 'jsonwebtoken';
 export default function authenticate(req, res, next){
 	const authorizationHeader = req.headers['authorization'];
 	let token;
-	
+
 	if(authorizationHeader){
 		token = authorizationHeader.split(' ')[1];
 	}
-	
+
 	if(!token){
 		res.status(401).send();
     return false;
   }
-  
+
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if(err){
       res.status(401).send();
       return false;
     }
     const User = req.app.db.models.User;
-    User.findOne({_id: decoded._id}).exec((err, user) => {
+    User.findOne({_id: decoded._id}).lean().exec((err, user) => {
       if(err){
         console.log(err);
         res.status(500).send('Ошибка сервера');
