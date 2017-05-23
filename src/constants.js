164d3860587fd0929e668dee5b00ae6ffd919eb0
mozjiqok <moskxxi@gmail.com>
2017-05-23 17:01:07 +1000

    Rearranged lines

--- src/constants.js
+++ src/constants.js
@@ -1,38 +1,28 @@
 import moment from 'moment'
 
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
-export const FETCH_CURRENT_USER_URI = '/api/user/logged_in_user';
-
-export const USER_CRUD = '/api/user/';
-
-export const USER_TEAM = '/api/user/get/team';
-
-export const NOTIFICATIONS_CRUD = '/api/notification/';
 export const FETCH_NOTIFICATIONS = 'FETCH_NOTIFICATIONS';
 export const MARK_NOTIFICATION = 'MARK_NOTIFICATION';
 export const MARK_ALL_NOTIFICATIONS = 'MARK_ALL_NOTIFICATIONS';
 export const SET_SYSTEM_ALERT = 'SET_SYSTEM_ALERT';
 export const FETCH_COMPANIES = 'FETCH_COMPANIES';
 export const FETCH_CURRENT_USER = 'FETCH_CURRENT_USER';
-
-export const TIME_ENTRY_CRUD = '/api/timeEntry/';
-
-export const FETCH_REDMINE_TIME_ENTRIES_URI = '/api/sync/redmine';
-export const FETCH_UPWORK_TIME_ENTRIES_URI = '/api/sync/upwork';
-
-export const REPORT_API_URI = '/api/report/';
-
-export const COMPANY_CRUD = '/api/company/';
-export const TEAM_CRUD = '/api/teams'
-
 export const FETCH_TEAMS = 'FETCH_TEAMS'
-
 export const ADD_TEAM_MEMBERS = 'ADD_TEAM_MEMBERS'
 export const ADD_TEAM = 'ADD_TEAM'
 export const DELETE_TEAM_MEMBERS = 'DELETE_TEAM_MEMBERS'
-
 export const DELETE_TEAM = 'DELETE_TEAM'
 export const CHANGE_TEAM_NAME = 'CHANGE_TEAM_NAME'
+export const TIME_ENTRY_CRUD = '/api/timeEntry/';
+export const FETCH_REDMINE_TIME_ENTRIES_URI = '/api/sync/redmine';
+export const FETCH_UPWORK_TIME_ENTRIES_URI = '/api/sync/upwork';
+export const REPORT_API_URI = '/api/report/';
+export const COMPANY_CRUD = '/api/company/';
+export const TEAM_CRUD = '/api/teams'
+export const FETCH_CURRENT_USER_URI = '/api/user/logged_in_user';
+export const USER_CRUD = '/api/user/';
+export const USER_TEAM = '/api/user/get/team';
+export const NOTIFICATIONS_CRUD = '/api/notification/';
 
 export function COMBINE_USER_ADDPHOTO_URI(login) {
   return '/api/user/' + login + '/upload_profile_picture';
