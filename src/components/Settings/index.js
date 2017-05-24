f011a02f55729e6b51663f4cec91e2bf112086b8
mozjiqok <moskxxi@gmail.com>
2017-05-25 00:20:31 +1000

    Change settings layout

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,75 +1,28 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React from 'react';
 import SettingsLeftMenu from './SettingsLeftMenu.jsx';
 import SettingsAddNewCompany from './SettingsAddNewCompany.jsx';
 import SettingsCompanyProfile from './SettingsCompanyProfile.jsx';
 import SettingsUserProfile from './SettingsUserProfile.jsx';
 
-class Settings extends Component {
+class Settings extends React.Component {
 
-  state = {
-    activeSection: 'user-profile'
-  }
-
-  static PropTypes = {
-    user: PropTypes.object,
-    teams: PropTypes.array
-  }
-
-  /**
-   * Handler for change settings section
-   * @param e
-   */
-  onSettingsSectionChange = (e) => {
-    this.setState({
-      settingsActiveSection: e.target.getAttribute("data-settings-section")
-    });
-
-    this.getSettingsSection = this.getSettingsSection.bind(this);
-  }
-
-  /**
-   * Get settings section according to the active left menu item
-   * @return {XML|boolean}
-   */
   getSettingsSection = () => {
-    const { user } = this.props;
-    switch ( this.state.activeSection ) {
-      case 'user-profile':
-        return <SettingsUserProfile
-          user={user}
-        />;
-      case 'add-new-company':
+    switch ( window.location.hash ) {
+      case '#add-new-company':
         return <SettingsAddNewCompany />;
-      case 'company-profile':
-        return (
-          <SettingsCompanyProfile
-            user={user}
-          />
-        );
+      case '#company-profile':
+        return <SettingsCompanyProfile />;
       default:
-        return false;
+        return <SettingsUserProfile />;
     }
   }
 
-  /**
-   * React Render
-   * @return {XML}
-   */
   render() {
-    const {user, companies} = this.props;
-
     return (
       <div className="container container__flex1 container__fixed">
         <div className="row">
           <div className="col-xs-12 col-sm-4 col-md-3">
-            <SettingsLeftMenu
-              activeSection={this.state.activeSection}
-              onSectionChange={this.onSectionChange}
-              user={user}
-              companies={companies}
-            />
+            <SettingsLeftMenu />
           </div>
           <div className="col-xs-12 col-sm-8 col-md-9">
             <div className="settings__content">
@@ -83,17 +36,4 @@ class Settings extends Component {
 
 }
 
-/**
- * Redux get parameters
- * @param state
- * @return {{user: {Object}, teams: {Array}}}
- */
-function getParams(state) {
-  return {
-    user: state.users[state.currentUser._id],
-    teams: state.teams,
-    companies: state.companies
-  }
-}
-
-export default connect(getParams)(Settings)
+export default Settings
