ab7a7884eb9f93821a0c1c6469056de4f116ae34
mozjiqok <moskxxi@gmail.com>
2017-05-26 00:10:29 +1000

    #3826 get rid of unneeded routes

--- server/router/users/index.js
+++ server/router/users/index.js
@@ -6,12 +6,7 @@ const router = express.Router();
 router.route('/')
   .get(handle.getAllUsers)
   .post(handle.saveUserToDb);
-router.route('/get/team')
-  .get(handle.getTeamUsers);
-router.route('/logged_in_user')
-  .get(handle.getLoggedInUser);
 router.route('/:_id')
-  .get(handle.showUser)
   .put(handle.updateUser)
   .delete(handle.deleteUser);
 router.route('/:_id/upload_profile_picture')
