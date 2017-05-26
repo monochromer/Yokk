919ef272d1e36236f30c35887c731c409e880868
mozjiqok <moskxxi@gmail.com>
2017-05-27 00:05:46 +1000

    #3613 Add update company case for companies reducer

--- src/reducers/companies.js
+++ src/reducers/companies.js
@@ -1,4 +1,4 @@
-import { FETCH_COMPANIES } from '../constants';
+import { FETCH_COMPANIES, UPDATE_COMPANY } from '../constants';
 
 const initialState = [];
 
@@ -6,6 +6,27 @@ export default function (state = initialState, action) {
   switch (action.type) {
     case FETCH_COMPANIES:
       return action.payload
+    case UPDATE_COMPANY:
+      let index = -1;
+      for(let i = 0; i < state.length; i++){
+        if (state[i]._id === action.companyId) {
+          index = i;
+          break;
+        }
+      }
+      if(index > -1){
+        return [
+          ...state.slice(0, index),
+          {
+            ...state[index],
+            ...action.payload
+          },
+          ...state.slice(index + 1)
+        ]
+      }
+      else{
+        return state;
+      }
     default:
       return state;
   }
