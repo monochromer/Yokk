ff9ad85a7b29ee709bee3e38c1700836ee3e258d
mozjiqok <moskxxi@gmail.com>
2017-05-26 23:59:23 +1000

    #3608 #3609 Add change password and invite member modals to modal root

--- src/components/modals/ModalRoot.jsx
+++ src/components/modals/ModalRoot.jsx
@@ -1,23 +1,34 @@
 import {
-  ADD_TEAM
+  ADD_TEAM,
+  CHANGE_PASSWORD,
+  INVITE_MEMBER
 } from '../../constants';
 import React from 'react';
 import { connect } from 'react-redux';
 import AddTeamModal from './AddTeamModal.jsx';
+import ChangePasswordModal from './ChangePasswordModal.jsx';
+import InviteMemberModal from './InviteMemberModal.jsx';
+import { hideModal } from '../../actions/modals'
 
 const MODAL_COMPONENTS = {
-  [ADD_TEAM]: AddTeamModal
+  [ADD_TEAM]: AddTeamModal,
+  [CHANGE_PASSWORD]: ChangePasswordModal,
+  [INVITE_MEMBER]: InviteMemberModal
 }
 
-const ModalRoot = ({ modalType, modalProps }) => {
+const ModalRoot = ({ modalType, modalProps, hideModal }) => {
   if (!modalType) {
     return <span />
   }
 
   const SpecificModal = MODAL_COMPONENTS[modalType]
-  return <SpecificModal {...modalProps} />
+  return <SpecificModal
+    {...modalProps}
+    hideModal={hideModal}
+  />
 }
 
 export default connect(
-  state => state.modals
+  state => state.modals,
+  { hideModal }
 )(ModalRoot)
