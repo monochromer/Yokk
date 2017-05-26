0e24c8180a119ade708a7f4fa6632c956a119990
mozjiqok <moskxxi@gmail.com>
2017-05-27 00:02:38 +1000

    #3590 Add company profile settings page

--- src/components/Settings/SettingsCompanyProfile.jsx
+++ src/components/Settings/SettingsCompanyProfile.jsx
@@ -1,37 +1,134 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { updateCompany } from '../../actions/companies';
+import { Input } from '../UI.jsx'
 
-/**
- * Component for view and edit company information
- */
-class SettingsCompanyProfile extends Component {
+class SettingsCompanyProfile extends React.Component {
 
-  /**
-   * Constructor
-   * @param props
-   */
-  constructor(props) {
-    super(props);
+  componentWillMount(){
+    this.setState(this.initialState);
   }
 
-  static PropTypes = {
-    user: PropTypes.object.isRequired
+  initialState = {
+    name: this.props.company.name,
+    address: this.props.company.address,
+    billingInfo: this.props.company.billingInfo,
+    errors: {}
   }
 
-  /**
-   * React Render
-   * @return {XML}
-   */
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.updateCompany(this.props.company._id, this.state, (err) => {
+      if(err){
+        this.setState({
+          errors: err
+        });
+        return false;
+      }
+      this.setState({
+        errors: {}
+      });
+      this.initialState = {
+        name: this.props.company.name,
+        address: this.props.company.address,
+        billingInfo: this.props.company.billingInfo,
+        errors: {}
+      }
+    });
+  }
+
+  handleCancel = (e) => {
+    e.preventDefault();
+    this.setState(this.initialState);
+  }
+
   render() {
-    const {user} = this.props;
-    console.log(user);
-    return(
-      <div className="settings-company-profile">
-        <h1>Company Profile</h1>
+    const {
+      name,
+      address,
+      billingInfo,
+      errors
+    } = this.state;
+    return (
+      <div>
+        <h1>Company profile</h1>
+        <form
+          className="settings-add-new-company__form"
+          onSubmit={this.handleSubmit}
+        >
+            <Input
+              handleChange={this.handleChange}
+              className="input-group input-group__grey"
+              label="Company Name *"
+              error={errors.name}
+              defaultValue={name}
+              name="name"
+            />
+            <Input
+              handleChange={this.handleChange}
+              className="input-group input-group__grey"
+              label="Company Address"
+              error={errors.address}
+              defaultValue={address}
+              name="address"
+            />
+          <div className="form-group billing-information">
+            <label htmlFor="companyBillingInformation">
+              Company Billing Information
+            </label>
+            <textarea
+              id="companyBillingInformation"
+              name="billingInfo"
+              rows="6"
+              value={billingInfo}
+              onChange={this.handleChange}
+            />
+            {
+              errors.billingInfo &&
+              <div className="form-error">{errors.billingInfo}</div>
+            }
+          </div>
+          {errors.form && <div className="form-error">{errors.form}</div>}
+          <div className="form-group">
+            <button
+              className="btn btn__md btn__blue"
+              type="submit"
+            >
+              Save
+            </button>
+            <button
+              className="btn btn__md"
+              type="submit"
+              onClick={this.handleCancel}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
       </div>
     );
   }
 
 }
 
-export default SettingsCompanyProfile;
+SettingsCompanyProfile.PropTypes = {
+  company: PropTypes.object.isRequired,
+  updateCompany: PropTypes.func.isRequired
+}
+
+function getParams(state) {
+  return {
+    company: state.companies.find(
+      el => el._id === state.users[state.currentUser._id].companyId
+    ) || {}
+  }
+}
+
+export default connect(getParams, { updateCompany })(SettingsCompanyProfile);
