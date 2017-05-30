f7be1a424555442a707b50f4144798a68a349286
mozjiqok <moskxxi@gmail.com>
2017-05-31 00:09:38 +1000

    #3856 Add alert and delete user modals to modal root

--- src/components/modals/ModalRoot.jsx
+++ src/components/modals/ModalRoot.jsx
@@ -1,19 +1,25 @@
 import {
   ADD_TEAM,
   CHANGE_PASSWORD,
-  INVITE_MEMBER
+  INVITE_MEMBER,
+  ALERT,
+  DELETE_USER
 } from '../../constants';
 import React from 'react';
 import { connect } from 'react-redux';
 import AddTeamModal from './AddTeamModal.jsx';
 import ChangePasswordModal from './ChangePasswordModal.jsx';
 import InviteMemberModal from './InviteMemberModal.jsx';
+import AlertModal from './AlertModal.jsx';
+import DeleteUserModal from './DeleteUserModal.jsx';
 import { hideModal } from '../../actions/modals'
 
 const MODAL_COMPONENTS = {
   [ADD_TEAM]: AddTeamModal,
   [CHANGE_PASSWORD]: ChangePasswordModal,
-  [INVITE_MEMBER]: InviteMemberModal
+  [INVITE_MEMBER]: InviteMemberModal,
+  [ALERT]: AlertModal,
+  [DELETE_USER]: DeleteUserModal
 }
 
 const ModalRoot = ({ modalType, modalProps, hideModal }) => {
