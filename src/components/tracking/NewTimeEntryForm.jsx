49ee84dacc1720965bf2a30957ff8ef90663ec93
mozjiqok <moskxxi@gmail.com>
2017-06-01 00:20:31 +1000

    #3570 change new time entry form

--- src/components/tracking/NewTimeEntryForm.jsx
+++ src/components/tracking/NewTimeEntryForm.jsx
@@ -1,156 +1,125 @@
-import React, { Component } from 'react'
-import store from '../../store.js'
-import moment from 'moment'
-import { createTimeEntry, fetchRedmineTimeEntries, fetchUpworkTimeEntries } from '../../actions/timeEntries.js'
-import { connect } from 'react-redux'
-import { findUserByEmail } from '../../helpers'
-import { validateString, validateDuration, validateDateFormat } from '../../utils/validators'
-import { Input } from '../UI.jsx'
+import React, { Component } from 'react';
+import moment from 'moment';
+import { createTimeEntry } from '../../actions/timeEntries.js';
+import { connect } from 'react-redux';
+import { validateDuration } from '../../utils/validators';
+import { Input } from '../UI.jsx';
+import DatePicker from 'react-datepicker';
+import { isEmpty } from 'lodash';
 
 class NewTimeEntryForm extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      description: {
-        value: '',
-        valid: false,
-        error: ''
-      },
-      duration: {
-        value: '00:00',
-        valid: false,
-        error: ''
-      },
-      date: {
-        value: moment().format('DD.MM.YYYY'),
-        valid: true,
-        error: ''
-      }
-    };
-
-    this.syncRedmine = this.syncRedmine.bind(this);
-    this.syncUpwork = this.syncUpwork.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  componentWillMount(){
+    this.setInitialState();
   }
 
-
-  syncRedmine() {
-    const user = findUserByEmail(this.props.users, this.props.currentUser);
-    if (user.redmineApiKey) {
-      store.dispatch(fetchRedmineTimeEntries());
-    } else {
-      alert('Check Redmine integration at your profile!');
-    }
-  }
-
-  syncUpwork() {
-    const user = findUserByEmail(this.props.users, this.props.currentUser);
-    if (user.upworkApiKey) {
-      store.dispatch(fetchUpworkTimeEntries());
-    } else {
-      alert('Check Upwork integration at your profile!');
-    }
-  }
-
-  handleChange(name, value) {
-    let valid, error;
-    switch(name) {
-      case 'description':
-        valid = validateString(value);
-        error = valid ? '' : 'Description is required.';
-        break;
-      case 'date':
-        valid = validateDateFormat(value);
-        error = valid ? '' : 'Invalid date.';
-        break;
-      case 'duration':
-        valid = validateDuration(value);
-        error = valid ? '' : 'Invalid duration.';
-        break;
-      default:
-    }
-
+  setInitialState = () => {
     this.setState({
-      [name]: { value, valid, error }
+      description: '',
+      duration: '00:00:00',
+      date: Date.now(),
+      errors: {}
     });
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: ("" + e.target.value).slice(0, 100),
+      errors: {
+        ...this.state.errors,
+        [e.target.name]: ''
+      }
+    });
+  }
 
+  handleDateChange = (e) => {
+    this.setState({
+      date: e.valueOf()
+    });
+  }
+
+  checkForm(){
+    const errors = {};
     const { description, duration, date } = this.state;
+    if(!description.length){
+      errors.description = 'Nothing to save!';
+    }
+    if(!validateDuration(duration)){
+      errors.duration = 'Incorrect duration.';
+    }
+    if(date < 1){
+      errors.date = 'Incorrect date.';
+    }
+    if(!isEmpty(errors)){
+      this.setState({errors});
+      return false;
+    }
+    this.setState({errors: {}});
+    return true;
+  }
 
-    if (!description.valid || !duration.valid || !date.valid) {
-      this.handleChange('description', description.value);
-      this.handleChange('duration', duration.value);
-      this.handleChange('date', date.value);
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (!this.checkForm()) {
       return;
     }
 
+    const { description, duration, date } = this.state;
+
+
     const timeEntry = {
-      date: moment(date.value, 'DD.MM.YYYY').valueOf(),
+      date,
       dateCreated: Date.now(),
-      description: description.value,
-      duration: duration.value,
+      description,
+      duration: moment.duration(duration).valueOf(),
       entrySource: 'eop',
       executor: this.props.currentUserID
     };
 
-    store.dispatch(createTimeEntry(timeEntry));
+    this.props.createTimeEntry(timeEntry);
   }
 
   render() {
+    const { description, date, errors } = this.state;
     return (
       <div className="container-fluid tracking-form">
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <div className="flex vertical-center text-center tracking-form_heading">
-              <div>
-                <h2 className="heading heading__white"> Track your time </h2>
-              </div>
-              <div>
-                <span className="tracking-form_or">or</span>
-              </div>
-              <button className="btn btn__md btn__trans-white" onClick={ this.syncRedmine }>Sync Redmine
-              </button>
-              <button className="btn btn__md btn__trans-white" onClick={ this.syncUpwork }>Sync Upwork
-              </button>
-            </div>
-          </div>
+        <div className="text-center heading heading__white">
+          Track your time
         </div>
         <form onSubmit={ this.handleSubmit }>
           <div className="container container__fixed">
             <div className="row tracking-form_row">
               <div className="col-md-8 tracking-form_description">
                 <Input className="input-group input-group__light-blue"
-                    handleChange={ event => this.handleChange('description', event.target.value) }
-                    error={ this.state.description.error }
+                    handleChange={this.handleChange}
+                    value={description}
+                    error={ errors.description }
                     name="description"
                     label="What you are working on?"/>
               </div>
               <div className="col-md-2 tracking-form_date">
-                <Input className="input-group input-group__light-blue"
-                    handleChange={ event => this.handleChange('date', event.target.value) }
-                    error={ this.state.date.error }
-                    defaultValue={ moment().format("DD.MM.YYYY") }
-                    name="date"
-                    label="Date"
-                    mask="99.99.9999"/>
+                <div className="input-group input-group__light-blue input-group__focus">
+                  <label>Date</label>
+                  <DatePicker
+                    selected={moment(date)}
+                    onChange={this.handleDateChange}
+                    title="Date of birth"
+                    value={moment(date).format('DD.MM.Y')}
+                  />
+                </div>
               </div>
 
               <div className="col-md-1 tracking-form_duration">
                 <Input className="input-group input-group__light-blue"
-                    handleChange={ event => this.handleChange('duration', event.target.value) }
-                    error={ this.state.duration.error }
+                    handleChange={this.handleChange}
+                    error={errors.duration}
                     name="duration"
-                    defaultValue="00:00"
+                    defaultValue="00:00:00"
                     label="Duration"
-                    mask="99:99"/>
+                    mask="99:99:99"/>
               </div>
               <div className="col-md-1 tracking-form_add">
-                <button className="btn btn__lg btn__white">Save</button>
+                <button className="btn btn__lg btn__white">Add</button>
               </div>
             </div>
           </div>
@@ -168,4 +137,4 @@ function getProps(store) {
   }
 }
 
-export default connect(getProps)(NewTimeEntryForm)
+export default connect(getProps, { createTimeEntry })(NewTimeEntryForm)
