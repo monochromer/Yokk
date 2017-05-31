82a5e50bbed91cbd3ab9abfa2f72d142bc55a930
mozjiqok <moskxxi@gmail.com>
2017-06-01 00:22:03 +1000

    #3571 Change time entries per day component

--- src/components/tracking/TimeEntriesPerDay.jsx
+++ src/components/tracking/TimeEntriesPerDay.jsx
@@ -1,34 +1,34 @@
 import React from 'react'
-import TimeEntryRow from './TimeEntryRow.jsx'
+import { durationBeatify } from '../../helpers'
+import TimeEntriesPerSource from './TimeEntriesPerSource.jsx'
 
 class TimeEntriesPerDay extends React.Component {
 
   render() {
-    let rows = this.props.timeEntries.map( (timeEntry) => {
-      return(<TimeEntryRow timeEntry={ timeEntry } key={ timeEntry._id } />);
-    });
+    const { sources, day } = this.props;
+    let rows = [];
+    for (let source in sources) {
+      let duration = durationBeatify(sources[source].totalDuration, 'short');
+      rows.push(
+        <TimeEntriesPerSource
+          source={ source }
+          duration={ duration }
+          timeEntries={ sources[source].list }
+          key={ source }
+        />
+      )
+    }
 
     return (
       <div className="row">
-        <div className="col-md-12 tracking-day">
-          <div className="row tracking-day_row">
+        <div className="col-md-12">
+          <div className="row">
             <div className="col-md-1"></div>
             <div className="col-md-4">
               <h3>
-                <span>
-                  { this.props.day }
-                </span>
-                <span className="tracking-day_duration">
-                  { this.props.duration }
-                </span>
+                { day }
               </h3>
             </div>
-          </div>
-          <div className="row tracking-table tracking-table_head">
-            <div className="col-md-1"></div>
-            <div className="col-md-9 tracking-table_heading">Description</div>
-            <div className="col-md-1 tracking-table_heading">Time</div>
-            <div className="col-md-1 tracking-table_heading"></div>
           </div>
           { rows }
         </div>
