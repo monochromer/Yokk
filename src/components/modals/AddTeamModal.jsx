266e4f6305049c4ccacc447c4b7b34401b396a9b
mozjiqok <moskxxi@gmail.com>
2017-06-01 18:03:44 +1000

    Finish fixing add new team modal #3576

--- src/components/modals/AddTeamModal.jsx
+++ src/components/modals/AddTeamModal.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { hideModal } from '../../actions/modals'
 import { Input } from '../UI.jsx'
 import { addTeam } from '../../actions/teams';
+import { isEmpty } from 'lodash';
 
 class AddTeamModal extends React.Component {
 
@@ -17,8 +17,24 @@ class AddTeamModal extends React.Component {
     ]
   }
 
-  handleClose = () => {
+  handleClose = (e) => {
+    if(e && e.preventDefault){
+      e.preventDefault();
+    }
     this.props.hideModal();
+  }
+
+  checkForm = () => {
+    const { name } = this.state;
+    const errors = {};
+    if(!name.length){
+      errors.name = 'Nothing to save!';
+    }
+    this.setState({errors});
+    if(isEmpty(errors)){
+      return true;
+    }
+    return false;
   }
 
   handleChange = (e) => {
@@ -27,9 +43,64 @@ class AddTeamModal extends React.Component {
     });
   }
 
-  handleSubmit(event) {
+  handleChangeUser = (e) => {
+    const index = parseInt(e.target.name, 10);
+    if (index < 0){
+      return;
+    }
+    const userId = e.target.value;
+    this.setState({
+      invites: [
+        ...this.state.invites.slice(0, index),
+        {
+          ...this.state.invites[index],
+          userId
+        },
+        ...this.state.invites.slice(index + 1),
+      ]
+    });
+  }
+
+  handleChangeRole = (e) => {
+    const index = parseInt(e.target.name, 10);
+    if (index < 0){
+      return;
+    }
+    const role = e.target.value;
+    this.setState({
+      invites: [
+        ...this.state.invites.slice(0, index),
+        {
+          ...this.state.invites[index],
+          role
+        },
+        ...this.state.invites.slice(index + 1),
+      ]
+    });
+  }
+
+  handleSubmit = (event) => {
     event.preventDefault();
-    this.props.addTeam(this.state.name);
+    if(!this.checkForm()) return;
+    this.props.addTeam(this.state, (err) => {
+      if(err){
+        this.setState({errors: err});
+        return;
+      }
+      this.handleClose();
+    });
+  }
+
+  handleAddInvite = () => {
+    this.setState({
+      invites: [
+        ...this.state.invites,
+        {
+          userId: 0,
+          role: 'user'
+        }
+      ]
+    });
   }
 
   render() {
@@ -48,21 +119,24 @@ class AddTeamModal extends React.Component {
     const invitationRows = invites.map((invite, index) => {
       return(
         <div className="row marginTop" key={index}>
-          <div className="col-md-6 input-group">
+          <div className="col-md-6">
             <select
               value={invite.userId}
               onChange={this.handleChangeUser}
+              name={index}
             >
+              <option value={0}>Select...</option>
               {userOptions}
             </select>
           </div>
-          <div className="col-md-6 input-group">
+          <div className="col-md-6">
             <select
               value={invite.role}
               onChange={this.handleChangeRole}
+              name={index}
             >
+              <option value="manager">Manager</option>
               <option value="user">User</option>
-              <option value="admin">Admin</option>
             </select>
           </div>
         </div>
@@ -89,19 +163,28 @@ class AddTeamModal extends React.Component {
                       label="Team name *"/>
                   </div>
                 </div>
-                <div className="row  marginTop">
-                  <div className="col-md-6 input-group">
+                <div className="row marginTop">
+                  <div className="col-md-6">
                     <div>
                       Add member
                     </div>
                   </div>
-                  <div className="col-md-6 input-group">
+                  <div className="col-md-6">
                     <div>
                       Role
                     </div>
                   </div>
                 </div>
                 {invitationRows}
+                <div
+                  className="row marginTop"
+                  onClick={this.handleAddInvite}
+                >
+                  <div className="col-md-6">
+                    <span className="glyphicons glyphicons-plus">
+                    </span> Add another
+                  </div>
+                </div>
                 <div className="row marginTop">
                   <div className="col-md-6">
                     <button
@@ -115,6 +198,7 @@ class AddTeamModal extends React.Component {
                     <button
                       type="submit"
                       className="btn btn__white btn__lg"
+                      onClick={this.handleClose}
                     >
                       Cancel
                     </button>
@@ -135,4 +219,4 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps, { hideModal })(AddTeamModal)
+export default connect(mapStateToProps, { addTeam })(AddTeamModal)
